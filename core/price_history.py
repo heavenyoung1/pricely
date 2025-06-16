0669f10618b7e467037c309fab09726afd40ff6f
Evgeny <jacksonou3@gmail.com>
2025-06-16 14:01:16 +0300

    class PriceHistory has been implemented

--- core/price_history.py
+++ core/price_history.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+from datetime import datetime
 
 @dataclass
 class PriceHistory:
     id: str
     url: str
-    history: List[Tuple[int, float]]
+    history: List[Tuple[int, datetime]]
 
-    def add_price(price: int):
-        pass
+    def add_price(self, price: int) -> None:
+        self.history.append((price, datetime.now()))
 
-    def get_last_price():
-        pass
+    def get_last_price(self) -> Optional[int]:
+        return self.history[-1] if self.history else None
 
