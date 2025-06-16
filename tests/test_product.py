674a96f62f51d58bcd2bd951a84463dc9bca6948
Evgeny <jacksonou3@gmail.com>
2025-06-16 15:29:37 +0300

    Configured test for product

--- tests/test_product.py
+++ tests/test_product.py
@@ -11,8 +11,8 @@ def product():
         id='1234567890',
         url='https://ozon.ru/product/1234567890',
         name='Test Product',
-        price=9999,
-        last_updated=datetime(2025, 6, 16, 4, 20, 00)
+        price=990,
+        last_updated=datetime(1946, 6, 13, 4, 20, 00),
     )
 
 def test_product_initialization(product):
@@ -21,8 +21,9 @@ def test_product_initialization(product):
     assert product.name == "Test Product"
     assert product.price == 990
     assert product.last_updated == datetime(1946, 6, 13, 4, 20, 00)
+    
 
 def test_update_price(product):
     product.update_price(650)
     assert product.price == 650
-    assert product.last_updated(0) != 1946
+    assert product.last_updated > datetime(1946, 6, 13, 4, 20, 00)
