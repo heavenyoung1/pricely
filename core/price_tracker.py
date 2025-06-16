73127eaedda8bcf102f92d89d01e952c7bd2e8ab
Evgeny <jacksonou3@gmail.com>
2025-06-16 14:32:02 +0300

    method add_product has been implemented

--- core/price_tracker.py
+++ core/price_tracker.py
@@ -1,14 +1,31 @@
 from core.price_parser import PriceParser
 from core.price_monitor import PriceMonitor
-
+from adapters.ozon_adapter import OzonAdapter
+from adapters.wb_adapter import WBAdapter
+from core.price_cache import JSONPriceCache
 
 class PriceTracker:
-    def __init__(self, parser: PriceParser, monitor: PriceMonitor):
-        self.parser = parser
-        self.monitor = monitor
+    def __init__(
+                self,
+                bot_token: str,
+                driver_path: str,
+                cache_file: str = 'prices.json',
+                interval: int = 3600,
+    ):
+        adapters = {
+              'ozon': OzonAdapter(driver_path),
+              'wb': WBAdapter(driver_path),
+        }
 
-    def add_product():
-            pass
+        self.parser = PriceParser(adapters)
+        cache = JSONPriceCache(cache_file)
+        notifier = None # тут будет notifier
+        self.monitor = PriceMonitor(self.parser, cache, notifier, interval)
+
+    def add_product(self, url: str, marketplace: str) -> None:
+        product = self.parser.parse(url, marketplace)
+        self.monitor.cache.save_price(url, product.price)
+        return product
 
     def monitor_price():
             pass
