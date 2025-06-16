725c6f8ae22cc43cc9e1c04ced2a709eed482d79
Evgeny <jacksonou3@gmail.com>
2025-06-16 13:49:46 +0300

    JSONPriceCache has been implemented

--- core/price_cache.py
+++ core/price_cache.py
@@ -1,11 +1,27 @@
 from adapters.interfaces import ICache
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict
+import os
+import json
 
 @dataclass
 class JSONPriceCache(ICache):
-    def save_price(url: str, price: int) -> None:
-        pass
+    cache_file: str
+    cache: Dict[str, int] = None
 
-    def get_price(url: str) -> Optional[int]:
-        pass
+    def _post_init__(self):
+        self.cache = self._load_cache()
+
+    def _load_cache(self) -> Dict[str, int]:
+            if os.path.exists(self.cache_file):
+                with open(self.cache_file, 'r', encoding='utf-8') as f:
+                    return json.load(f)
+            return {}
+
+    def save_price(self, url: str, price: int) -> None:
+        self.cache[url] = price
+        with open(self.cache_file, 'w', encoding='utf-8') as f:
+             json.dump(self.cache, f, ensure_ascii=False, indent=2)
+
+    def get_price(self, url: str) -> Optional[int]:
+        return self.cache.get(url)
