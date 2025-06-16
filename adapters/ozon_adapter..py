264beee9d43dba4e04c1befb79c9dc3966bba9ce
Evgeny <jacksonou3@gmail.com>
2025-06-16 10:42:24 +0300

    The class OzonAdapter been cleaned up a bit

--- adapters/ozon_adapter..py
+++ adapters/ozon_adapter..py
@@ -3,13 +3,17 @@ from dataclasses import dataclass
 from models.product import Product
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from datetime import datetime
 
 @dataclass
 class OzonAdapter(IMarketPlace):
     driver_path: str
-    driver: webdriver
+    driver: webdriver.Chrome = None
 
+    def __post_init__(self):
+        self.driver = webdriver.Chrome(self.driver_path)
 
+    @staticmethod
     def extract_product_id(url: str) -> str:
         splitedStr = url.split("/")[-2]
         ID = splitedStr[-10:]
