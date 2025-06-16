3415ccd3fe2f0d6343034288b04acbbd8f109fcf
Evgeny <jacksonou3@gmail.com>
2025-06-16 16:47:14 +0300

    Test_price_history has been implemented, also get_last_price method was chenged, fixed correct return in tuple, tuple[-1][0] instead of tuplep[-1]

--- core/price_history.py
+++ core/price_history.py
@@ -13,5 +13,5 @@ class PriceHistory:
         self.history.append((price, datetime.now()))
 
     def get_last_price(self) -> Optional[int]:
-        return self.history[-1] if self.history else None #return self.history[-1][0] if self.history else None
+        return self.history[-1][0] if self.history else None
 
---
+++ tests/test_price_history.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import pytest
+import logging
+
+from core.price_history import PriceHistory
+
+
+@pytest.fixture
+def price_history():
+    return PriceHistory(
+        id='1234567890',
+        url='https://ozon.ru/product/1234567890',
+        history=[
+            (950, datetime(2025, 6, 13))
+        ]
+    )
+
+def test_price_history_initialization(price_history):
+    assert price_history.id == "1234567890"
+    assert price_history.url == "https://ozon.ru/product/1234567890"
+    assert price_history.history == [(950, datetime(2025, 6, 13))]
+
+def test_add_price(price_history):
+    price_history.add_price(930)
+    assert len(price_history.history) == 2
+    assert price_history.history[-1][0] == 930
+
+def test_get_last_price(price_history):
+    assert price_history.get_last_price() == 950
