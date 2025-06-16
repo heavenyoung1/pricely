45cb92c0bbfccfa17c8ed5c20f354fecd31174db
Evgeny <jacksonou3@gmail.com>
2025-06-16 15:20:01 +0300

    pytest.ini was created and writed 2 tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -14,3 +14,15 @@ def product():
         price=9999,
         last_updated=datetime(2025, 6, 16, 4, 20, 00)
     )
+
+def test_product_initialization(product):
+    assert product.id == "1234567890"
+    assert product.url == "https://ozon.ru/product/1234567890"
+    assert product.name == "Test Product"
+    assert product.price == 990
+    assert product.last_updated == datetime(1946, 6, 13, 4, 20, 00)
+
+def test_update_price(product):
+    product.update_price(650)
+    assert product.price == 650
+    assert product.last_updated(0) != 1946
