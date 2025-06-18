d58c3ad2a6cc967f0769f4f32290a3225853773c
Evgeny <jacksonou3@gmail.com>
2025-06-18 11:00:12 +0300

    tests for telegram bot has been implemented

--- tests/test_telegram_notifier.py
+++ tests/test_telegram_notifier.py
@@ -1,5 +1,5 @@
-from unittest.mock import AsyncMock, patch
 import pytest
+from unittest.mock import AsyncMock, Mock, patch
 from datetime import datetime
 
 from core.telegram_notifier import TelegramNotifier
@@ -7,30 +7,35 @@ from models.product import Product
 
 
 @pytest.fixture
-def telegram_notifier():
-    with patch('pricetracker.core.telegram_notifier.Bot') as mock_bot:
-        notifier = TelegramNotifier(bot_token='fake_token')
-        notifier.bot = AsyncMock()
-        notifier.bot.send_message = AsyncMock()
-        return notifier
-
-@pytest.fixture
-def sample_product():
+def product():
     return Product(
-        id='123',
+        id='1234567889',
         url='https://ozon.ru/product/1234567889',
         name='Test Product',
         price=900,
         last_updated=datetime(2025, 6, 16)
     )
 
+@pytest.fixture
+def notifier():
+    with patch('core.telegram_notifier.Bot') as MockBot:
+        mock_bot_instance = Mock()
+        mock_bot_instance.send_message = AsyncMock()
+
+        MockBot.return_value = mock_bot_instance
+
+        notifier = TelegramNotifier(bot_token='fake_token')
+        return notifier
+
 @pytest.mark.asyncio
-async def test_notify(telegram_notifier, sample_product):
-    await telegram_notifier.notify(chat_id=12345, product=sample_product, old_price=1000.0)
-    telegram_notifier.bot.send_message.assert_called_once()
-    args, kwargs = telegram_notifier.bot.send_message.call_args
-    assert kwargs['chat_id'] == 12345
-    assert 'Цена изменилась' in kwargs['text']
-    assert 'Test Product' in kwargs['text']
-    assert 'Старая цена: 1000' in kwargs['text']
-    assert 'Новая цена: 900' in kwargs['text']
+async def test_notify(notifier, product):
+    old_price = 1000
+    await notifier.notify(chat_id=12345, product=product, old_price=old_price)
+
+    notifier.bot.send_message.assert_called_once()
+    message = notifier.bot.send_message.call_args.kwargs['text']
+
+    assert 'Цена изменилась' in message
+    assert 'Test Product' in message
+    assert f'Старая цена: {old_price}' in message
+    assert f'Новая цена: {product.price}' in message
