71289893e06ad5a6275ba012b31ce194a8d35603
Evgeny <jacksonou3@gmail.com>
2025-06-23 14:23:07 +0300

    Method track_product has been implemented for class ProductTracker

--- core/services/tracker.py
+++ core/services/tracker.py
@@ -19,28 +19,43 @@ class ProductTracker:
         self.parser_factory = parser_factory
 
     def track_product(self, user: User, url: str) -> None:
-        being_product = self.product_repo.find_by_url(url)
-        if being_product:
-            user.follow(being_product)
-            self.user_repo.save(user)
-            self.notifier.notify(user, f'Вы, {user.telegram_id}, подписаны на товар {being_product.na}')
-            return
-        
-        # Парсим товар
-        parser = self.parser_factory.get_parser(url)
-        product_info = parser.parse(url)
+        """ Добавляет товар на отслеживание для пользователя """
+        try:
+            # Проверяем, существует ли уже товар
+            being_product = self.product_repo.find_by_url(url)
+            if being_product:
+                user.follow(being_product)
+                self.user_repo.save(user)
+                self.notifier.notify(user, f'Вы, {user.telegram_id}, подписаны на товар {being_product.na}')
+                return
+            
+            # Парсим товар
+            parser = self.parser_factory.get_parser(url)
+            product_info = parser.parse(url)
 
-        # Создаём новый товар
-        product = Product(
-            id=product_info.id, #Далее переопределить метод для парсинга ID товара
-            name=product_info.name,
-            url=product_info.url,
-            marketplace=parser.get_marketplace(),
-            last_price=product_info.last_price,
-            image_url=product_info.image_url,
-        )
+            # Создаём новый товар
+            product = Product(
+                id=product_info.id, #Далее переопределить метод для парсинга ID товара
+                name=product_info.name,
+                url=product_info.url,
+                marketplace=parser.get_marketplace(),
+                last_price=product_info.last_price,
+                image_url=product_info.image_url,
+            )
 
-        
+            # Сохраняем товар и подписку пользователя
+            self.product_repo.save(product)
+            user.follow(product)
+            self.user_repo.save(product)
+
+            # Уведомляем пользователя о подписке
+            self.notifier.notify(user, f'Добавлен товар для отслеживания f{product.name}')
+        except Exception as e:
+            error_msg = f'Ошибка при добавлении товара {product.name}'
+            raise RuntimeError(error_msg)
+
+    def check_price():
+        pass
 
 
 
