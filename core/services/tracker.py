19de05c470df5ffefbb41d16e3f385b6dab9b434
Evgeny <jacksonou3@gmail.com>
2025-06-23 10:20:15 +0300

    Created __init__ for ProductTracker

--- core/services/tracker.py
+++ core/services/tracker.py
@@ -1,4 +1,17 @@
-
+from core.interfaces.notifier import INotifier
+from core.interfaces.parser import IProductParserFactory
+from core.interfaces.product import IProductRepo
+from core.interfaces.user import IUserRepo
 
 class ProductTracker:
-    pass
+    def __init__(
+            self, 
+            user_repo: IUserRepo,
+            product_repo: IProductRepo,
+            notifier: INotifier,
+            parser_factory: IProductParserFactory
+            ):
+        self.user_repo = user_repo
+        self.product_repo = product_repo
+        self.notifier = notifier
+        self.parser_factory = parser_factory
