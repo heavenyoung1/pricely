6c7f3f1282aaff992f651a45cf95fb6bc21bcb8c
Evgeny <jacksonou3@gmail.com>
2025-06-23 09:46:52 +0300

    Created Fatory for IProductParser

--- core/interfaces/parser.py
+++ core/interfaces/parser.py
@@ -4,5 +4,10 @@ from core.models.product import Product
 
 class IProductParser(ABC):
     @abstractmethod
-    def parse(self, url: str) -> Product:
+    def parse(self, url: str) -> 'Product':
+        pass
+
+class IProductParserFactory(ABC):
+    @abstractmethod
+    def get_parser(self, url: str) -> IProductParser:
         pass
