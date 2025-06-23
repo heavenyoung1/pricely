f6277ebdfaa819930907aecdec53ca4c3fab55ed
Evgeny <jacksonou3@gmail.com>
2025-06-23 10:32:54 +0300

    Added Logger

--- core/services/tracker.py
+++ core/services/tracker.py
@@ -2,6 +2,7 @@ from core.interfaces.notifier import INotifier
 from core.interfaces.parser import IProductParserFactory
 from core.interfaces.product import IProductRepo
 from core.interfaces.user import IUserRepo
+from core.models.user import User
 
 class ProductTracker:
     def __init__(
@@ -15,3 +16,8 @@ class ProductTracker:
         self.product_repo = product_repo
         self.notifier = notifier
         self.parser_factory = parser_factory
+
+    def track_product(self, user: User, url: str) -> None:
+        existing_product = self.product_repo.find_by_url(url)
+        
+
