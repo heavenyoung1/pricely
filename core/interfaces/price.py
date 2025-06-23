26b50e3eb0f9ea225fe9e84fd8e31d927ca5a0e3
Evgeny <jacksonou3@gmail.com>
2025-06-23 15:13:41 +0300

    Interface for Price has been implemented

---
+++ core/interfaces/price.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+from core.models.price import Price
+
+class IPriceRepo(ABC):
+    @abstractmethod
+    async def save(self, price: Price) -> None:
+        pass
