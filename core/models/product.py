be4108375c5d0fbb45b0f5dff5a949bccaa55961
Evgeny <jacksonou3@gmail.com>
2025-06-24 10:05:46 +0300

    Added new function make_price_record

--- core/models/product.py
+++ core/models/product.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
+from datetime import datetime
+from uuid import uuid4
 from typing import Dict, Optional
+
+from core.models.price import Price
 
 @dataclass
 class ProductInfo:
@@ -29,4 +33,12 @@ class Product:
         if changed:
             self.last_price = new_price
         return result
+    
+    def make_price_record(self) -> Price:
+        return Price(
+            id=str(uuid4()),
+            product_id=self.id,
+            price=self.last_price,
+            timestamp=datetime.now()
+    )
 
