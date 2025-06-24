8ab9b58867e85895407ad4364d0bb2318c6427a4
Evgeny <jacksonou3@gmail.com>
2025-06-24 13:49:45 +0300

    changes

--- adapters/wb.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from typing import Optional
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from seleniumwire import webdriver
-from core.interfaces.product import IProductParser, ProductInfo
-from core.models.market import Marketplace
