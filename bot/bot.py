10f7414c60a49fb3da9cf4ab6423c466418cb226
Evgeny <jacksonou3@gmail.com>
2025-06-24 10:37:34 +0300

    configure app

--- bot/bot.py
+++ bot/bot.py
@@ -6,10 +6,11 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from core.interfaces.notifier import INotifier
+from core.models.user import User
 
 class TelegramNotifier(INotifier):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    async def notify(self, user, message) -> None:
+    async def notify(self, user: User, message: str) -> None:
         await self.bot.send_message(chat_id=user.telegram_id, text=message)
