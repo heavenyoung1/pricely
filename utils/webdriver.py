017cb2b649fca01dc0186bd0961e991fa433c534
Eugene <jacksonou3@gmail.com>
2025-07-01 11:07:06 +0300

    little refactoring

--- utils/webdriver.py
+++ utils/webdriver.py
@@ -1,23 +1,42 @@
-from selenium import webdriver
-from selenium_stealth import stealth
+# from selenium import webdriver
+# from selenium_stealth import stealth
 
-def init_driver(): 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
+# def init_driver(): 
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--start-maximized")
+#     options.add_argument("--disable-blink-features=AutomationControlled")
+#     options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#     options.add_experimental_option("useAutomationExtension", False)
     
-    driver = webdriver.Chrome(options=options)
+#     driver = webdriver.Chrome(options=options)
     
-    stealth(driver,
-            languages=["en-US", "en"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+#     stealth(driver,
+#             languages=["en-US", "en"],
+#             vendor="Google Inc.",
+#             platform="Win32",
+#             webgl_vendor="Intel Inc.",
+#             renderer="Intel Iris OpenGL Engine",
+#             fix_hairline=True,
+#             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
     
+#     return driver
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.ozon.ru/')
+
+title = driver.title
+
+driver.implicitly_wait(0.5)
+
+driver.quit()
+
+def setup():
+    driver = webdriver.Chrome()
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
     return driver
 
+def teardown(driver):
+    driver.quit()
