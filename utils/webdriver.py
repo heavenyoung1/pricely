c8c8fac23936eb4cac010301d791377f39dc24ec
Eugene <jacksonou3@gmail.com>
2025-07-01 12:42:04 +0300

    implemented basic driver configuration on selenius

--- utils/webdriver.py
+++ utils/webdriver.py
@@ -1,42 +1,36 @@
-# from selenium import webdriver
-# from selenium_stealth import stealth
-
-# def init_driver(): 
-#     options = webdriver.ChromeOptions()
-#     options.add_argument("--start-maximized")
-#     options.add_argument("--disable-blink-features=AutomationControlled")
-#     options.add_experimental_option("excludeSwitches", ["enable-automation"])
-#     options.add_experimental_option("useAutomationExtension", False)
-    
-#     driver = webdriver.Chrome(options=options)
-    
-#     stealth(driver,
-#             languages=["en-US", "en"],
-#             vendor="Google Inc.",
-#             platform="Win32",
-#             webgl_vendor="Intel Inc.",
-#             renderer="Intel Iris OpenGL Engine",
-#             fix_hairline=True,
-#             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
-    
-#     return driver
-
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
 
-driver = webdriver.Chrome()
+def init_driver(headless= None, user_agent=None, proxy=None , wait_time=10):
+    options = Options()
 
-driver.get('https://www.ozon.ru/')
+    # Базовые настройки Chrome
+    if headless:
+        options.add_argument("--headless") # Режим, при котором браузер работает без графического интерфейса
 
-title = driver.title
+    options.add_argument("--no-sandbox") # Отключает песочницу (sandbox) для работы под root (например, в Docker).
+    options.add_argument("--disable-gpu") # Отключает использование GPU
+    options.add_argument("--disable-blink-features=AutomationControlled") # Скрывает признаки автоматизации (например, убирает флаг navigator.webdriver = true).
+    options.add_argument("--start-maximized") # Запускает браузер в максимальном размере окна
 
-driver.implicitly_wait(0.5)
+    # Пользовательские настройки
+    if user_agent:
+        options.add_argument(f'user-agent={user_agent}') # Имитация разных устройств (ПК, мобильные)
+    if proxy:
+        options.add_argument(f'--proxy-server={proxy}')
 
-driver.quit()
+    # Инициализация драйвера
+    driver = webdriver.Chrome(options=options)
 
-def setup():
-    driver = webdriver.Chrome()
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    # Stealth-настройки (используем переданный user_agent или дефолтный)
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            user_agent=user_agent or "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+    
     return driver
-
-def teardown(driver):
-    driver.quit()
