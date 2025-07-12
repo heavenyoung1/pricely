28ec82ca10c2503ee40da4aa5a44253604babb66
Eugene <jacksonou3@gmail.com>
2025-07-12 13:26:30 +0300

    Base method check_price_change has benn implmented

--- use_cases/price_tracker_use_cases.py
+++ use_cases/price_tracker_use_cases.py
@@ -14,3 +14,13 @@ class PriceTrackerUseCase:
         self.urls = urls                    # Список URL для мониторинга
         self.chat_id = chat_id              # ID чата Telegram
         self.bot_token = bot_token          # Токен бота Telegram
+
+    def check_price_change(self, url: str):
+        '''Проверяет изменение цены для заданного URL'''
+        try:
+            logger.info(f'Проверка цены для URL: {url}')
+            product = self.repository.get_product_data(url)
+            previous_price = self.state_manager.get_previous_price(url)
+            current_price = product.price_without_card
+        except Exception as e:
+            logger.error(f"Ошибка при проверке цены для {url}: {e}")
