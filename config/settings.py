3c3e6794ebf7d6461d8a439256ee2ce1563003e9
Eugene <jacksonou3@gmail.com>
2025-07-12 22:44:03 +0300

    More functions for furrther work

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,15 @@
 from dotenv import load_dotenv
 import os
 
+from utils.logger import logger
+
 # Загружаем переменные из .env
 load_dotenv()
 
 # Telegram
 bot_token = os.getenv("BOT_TOKEN")
 chat_id = os.getenv("CHAT_ID")
+
+if not bot_token or not chat_id:
+    logger.error('Отсутствуют BOT_TOKEN или CHAT_ID в .env файле')
+    raise ValueError("Отсутствуют BOT_TOKEN или CHAT_ID в .env файле")
