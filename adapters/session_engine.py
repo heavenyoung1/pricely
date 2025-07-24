edd72029ce4c338af0e38c832e68156e37233feb
Evgeny <jacksonou3@gmail.com>
2025-07-24 15:27:05 +0300

    SessionEngine created

--- adapters/session_engine.py
+++ adapters/session_engine.py
@@ -7,3 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from config.settings import settings
 from infrastructure.parsers.selenium_driver import driver
 
+# Настройка логгера
+logger = logging.getLogger(__name__)
+
+class SessionEngine:
+    '''Класс для управления сессией браузера с помощью Selenium WebDriver'''
+
+    def __init__(self, 
+                headless: bool = settings.SELENIUM_HEADLESS, 
+                user_agent: str = settings.DEFAULT_USER_AGENT, 
+                proxy: str = None, 
+                wait_time: int = settings.SELENIUM_WAIT_TIME):
+        '''Инициализация сессии с WebDriver'''
+        self.driver = driver(headless=headless, user_agent=user_agent, proxy=proxy, wait_time=wait_time)
+        self.wait_time = wait_time
+        logger.info("SessionEngine инициализирован")
+        
