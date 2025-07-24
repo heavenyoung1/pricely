d0a35729309e12c1144852452fc642328d180399
Evgeny <jacksonou3@gmail.com>
2025-07-24 11:41:20 +0300

    Changing driver

--- infrastructure/parsers/driver_factory.py
+++ infrastructure/parsers/driver_factory.py
@@ -4,22 +4,20 @@ from selenium.webdriver.chrome.options import Options
 
 from config.settings import settings
 
+
 def apply_stealth_settings(driver, user_agent: str = None) -> None:
     """
     Применяет stealth-настройки для маскировки Selenium WebDriver.
-    
+
     :param driver: Экземпляр Selenium WebDriver
     :param user_agent: Пользовательский user-agent (если не указан — используется дефолтный)
     """
+    stealth_settings = settings.SELENIUM_STEALTH_SETTINGS.copy() # Копируем, чтобы не изменять оригинал??
+    stealth_settings['user_agent'] = user_agent or settings.DEFAULT_USER_AGENT
 
-    stealth(driver,
-        languages=["en-US", "en"],
-        vendor="Google Inc.",
-        platform="Win32",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-        user_agent=user_agent,
+    stealth(
+        driver,
+        **stealth_settings,
     )
 
 def init_driver(headless: bool = settings.SELENIUM_HEADLESS, 
@@ -28,36 +26,22 @@ def init_driver(headless: bool = settings.SELENIUM_HEADLESS,
                 wait_time: int = settings.SELENIUM_WAIT_TIME) -> webdriver.Chrome:
     """
     Инициализирует Chrome WebDriver с поддержкой stealth-настроек.
-    
-    :param headless: Запуск без графического интерфейса
-    :param user_agent: Пользовательский user-agent
-    :param proxy: Прокси-сервер (http://host:port)
-    :param wait_time: Зарезервировано для будущих фич
     :return: Настроенный экземпляр WebDriver
     """
     options = Options()
 
-    # Основные аргументы
-    chrome_args = [
-        "--no-sandbox",                                   # Отключает песочницу (sandbox) для работы под root
-        "--disable-gpu",                                  # Отключает использование GPU
-        "--disable-blink-features=AutomationControlled",  # Скрывает признаки автоматизации
-        "--start-maximized",                              # Запускает браузер в максимальном размере окна
-        "--log-level=3",                                  # Уровень 3 подавляет большинство сообщений
+    chrome_args = settings.SELENIUM_CHROME_ARGS.copy() # Копируем, чтобы не изменять оригинал
+    optional_args = settings.SELENIUM_CHROME_ARGS.copy()
+    optional_args = [
+        "--headless=new" if headless else None,
+        f"--user-agent={user_agent}" if user_agent else None,
+        f"--proxy-server={proxy}" if proxy else None,
     ]
-
-    if headless:
-        chrome_args.append("--headless=new")
-    if user_agent:
-        chrome_args.append(f"--user-agent={user_agent}")
-    if proxy:
-        chrome_args.append(f"--proxy-server={proxy}")
+    chrome_args.extend([arg for arg in optional_args if arg is not None])
     
     for arg in chrome_args:
         options.add_argument(arg)
 
     driver = webdriver.Chrome(options=options)
-
     apply_stealth_settings(driver, user_agent)
-
     return driver
