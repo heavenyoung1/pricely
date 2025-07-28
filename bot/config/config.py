63c40f0982053ee10d45f0560b0a6da3e12ff228
Evgeny <jacksonou3@gmail.com>
2025-07-28 15:49:58 +0300

    The structure of the project is redesigned under the principle of clean architecture

--- bot/config/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
-
-class TelegramBotSetting(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
-
-    TOKEN: str = Field(..., alias="TOKEN_BOT")
-
-bot_setting = TelegramBotSetting()
---
+++ src/adapters/__init__.py
@@ -0,0 +1,1 @@
+# Interface Adapters (контроллеры, презентеры)
