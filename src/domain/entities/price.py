fee80770a006f8f55622a4f764673b2394d8f7e7
Evgeny <jacksonou3@gmail.com>
2025-07-28 16:38:12 +0300

    New Entities for project

---
+++ src/domain/entities/price.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import List
+from datetime import datetime
+
+@dataclass
+class PriceTag:
+    '''Сущность - Метка Цены'''
+    ID_stamp: int                       # ID клейма 
+    ID_product: str                     # ID продукта 
+    time_stamp: datetime                # Время клейма 
+    price_with_card: int
+    price_without_card: int
+    previous_price_without_card: int
+    price_default: int
---
+++ src/domain/entities/product.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Product:
+    '''Сущность Product (товар)'''
+    id: str
+    name: str
+    rating: float
+    price_with_card: int
+    price_without_card: int
+    previous_price_without_card: int
+    price_default: int
+    discount_amount: float
+    link: str
+    url_image: str
+    category_product: List[str]
