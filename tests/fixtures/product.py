3f679c0c296625d4d839d9b8c6259d33d73ca5cd
Evgeny <jacksonou3@gmail.com>
2025-07-29 15:26:24 +0300

    Test data for test moved to the special directory

---
+++ tests/fixtures/product.py
@@ -0,0 +1,25 @@
+import pytest
+from datetime import datetime
+
+@pytest.fixture
+def product_test():
+    return {
+            'id' :'1804652778',
+            'name': 'Чаша для кальяна глиняная',
+            'rating': 4.9,
+            'price_with_card': 500,
+            'price_without_card': 561,
+            #previous_price_without_card: int
+            'price_default': 899,
+            #discount_amount: float
+            'link': 'https://www.ozon.ru/product/chasha-dlya-kalyana-glinyanaya-cosmo-bowl-turkish-shot-1804652778/',
+            'url_image': 'https://ir.ozone.ru/s3/multimedia-1-9/wc1000/7352613513.jpg',
+            'category_product': [
+                'Товары для курения и аксессуары',
+                'Товары для курения',
+                'Аксессуары и комплектующие для кальянов',
+                'Комплектующие',
+                'Cosmo Bowl',
+                ],
+            'timestamp': datetime(2025, 1, 1, 1, 2, 3),
+    }
--- tests/unit/application/test_product_service.py
+++ tests/unit/application/test_product_service.py
@@ -1,37 +1,11 @@
 from datetime import datetime
 from unittest.mock import Mock
-from pydantic import HttpUrl
-import pytest
 import logging
 
 from src.application.services.product_service import ProductService
-from src.domain.entities.product import Product
+from tests.fixtures.product import product_test
 
 logger = logging.getLogger(__name__)
-
-@pytest.fixture
-def product_test():
-    return {
-            'id' :'1804652778',
-            'name': 'Чаша для кальяна глиняная',
-            'rating': 4.9,
-            'price_with_card': 500,
-            'price_without_card': 561,
-            #previous_price_without_card: int
-            'price_default': 899,
-            #discount_amount: float
-            'link': 'https://www.ozon.ru/product/chasha-dlya-kalyana-glinyanaya-cosmo-bowl-turkish-shot-1804652778/',
-            'url_image': 'https://ir.ozone.ru/s3/multimedia-1-9/wc1000/7352613513.jpg',
-            'category_product': [
-                'Товары для курения и аксессуары',
-                'Товары для курения',
-                'Аксессуары и комплектующие для кальянов',
-                'Комплектующие',
-                'Cosmo Bowl',
-                ],
-            'timestamp': datetime(2025, 1, 1, 1, 2, 3),
-    }
-
 
 def test_success_create_product(product_test):
     '''TEST - SUCCESSED CREATED PRODUCT'''
