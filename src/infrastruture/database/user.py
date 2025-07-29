bc508e4c01be625bdbc3c7f4cc117a76560e1965
Eugene <jacksonou3@gmail.com>
2025-07-29 21:58:49 +0300

    DBUser has been modified

--- src/infrastruture/database/user.py
+++ src/infrastruture/database/user.py
@@ -1,18 +1,20 @@
-from src.infrastruture.database.base import Base
-
-from datetime import datetime
 from typing import List
-from typing import Optional
-from sqlalchemy import ForeignKey, String, Integer, Float, DateTime, JSON
-from sqlalchemy.orm import DeclarativeBase
-
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
+from src.infrastruture.database.base import Base
+
 class DBUser(Base):
+    '''Модель пользователя для БД.
+    
+    Attributes:
+        id: Уникальный идентификатор пользователя
+        name: Имя пользователя
+        products: Список продуктов пользователя (отношение один-ко-многим)
+    '''
     __tablename__ = 'users'
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
-    product_id: Mapped[str] = mapped_column(String, ForeignKey('product.product_id'), index=True)
+    name: Mapped[str] = mapped_column(String(50))
+    products: Mapped[List['DBProduct']] = relationship(back_populates="user")
