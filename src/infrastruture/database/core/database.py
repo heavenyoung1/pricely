e7bb1e7b99f188f7d1cfb382c25266d916b78592
Evgeny <jacksonou3@gmail.com>
2025-07-30 10:28:34 +0300

    DB connection has been configured

--- src/infrastruture/database/core/database.py
+++ src/infrastruture/database/core/database.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
+from typing import Generator
 
 class DataBaseSettings(BaseSettings):
     '''Настройки подключения к базе данных'''
@@ -20,14 +21,19 @@ class DataBaseSettings(BaseSettings):
 
     def get_connection_db(self):
         '''Формирует строку подключения для PostgreSQL'''
-        connection_str = f'{self.CONN}://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}'
-        return connection_str
+        return f'{self.CONN}://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}'
 
 # Инициализация настроек БД   
 db_settings = DataBaseSettings()
 
 # Создание движка (engine)
-engine = db_settings.get_connection_db()
+engine = create_engine(
+    db_settings.get_connection_string(),
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,
+    echo=False  # Включить для отладки SQL-запросов
+)
 
 # Фабрика сессий
 SessionFactory = sessionmaker(
@@ -35,3 +41,17 @@ SessionFactory = sessionmaker(
     autoflush=False,
     expire_on_commit=False,
 )
+
+# Генератор сессий
+def get_db() -> Generator[Session, None, None]:
+    """Генератор сессий для работы с БД"""
+    db = SessionFactory()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Инициализация базы данных (создание таблиц)
+def init_db():
+    from src.infrastruture.database.models.base import Base
+    Base.metadata.create_all(bind=engine)
