af518f1e466c63b0172098bcb51124ad68048d2d
Evgeny <jacksonou3@gmail.com>
2025-07-30 10:04:04 +0300

    Configured job db

--- src/config/database.py
+++ src/config/database.py
@@ -1,4 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine
 
 class DataBaseSettings(BaseSettings):
     '''Настройки подключения к базе данных'''
@@ -6,8 +8,7 @@ class DataBaseSettings(BaseSettings):
         env_file = '.env', 
         env_prefix='DB_CONFIG_', 
         env_file_encoding = 'utf-8',
-        extra="ignore", # Игнорировать лишние переменные
-
+        extra='ignore', # Игнорировать лишние переменные
         )
 
     HOST: str
@@ -19,6 +20,18 @@ class DataBaseSettings(BaseSettings):
 
     def get_connection_db(self):
         '''Формирует строку подключения для PostgreSQL'''
-        connection_str = f'{self.DB_CONN}://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        connection_str = f'{self.CONN}://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}'
         return connection_str
-    
+
+# Инициализация настроек БД   
+db_settings = DataBaseSettings()
+
+# Создание движка (engine)
+engine = db_settings.get_connection_db()
+
+# Фабрика сессий
+SessionFactory = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=False,
+)
