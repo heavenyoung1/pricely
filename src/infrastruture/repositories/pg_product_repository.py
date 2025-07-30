724f6dc7ee11539203a2ce27d6af0e244b4d8266
Eugene <jacksonou3@gmail.com>
2025-07-30 22:02:07 +0300

    bug fiixed

--- src/infrastruture/repositories/pg_product_repository.py
+++ src/infrastruture/repositories/pg_product_repository.py
@@ -18,31 +18,31 @@ class PGSQLProductRepository(ProductRepository):
     def __init__(self, session: Session):
         self.session = session
 
-        def save_one_product(self, product: Product, price_stamp: PriceStamp) -> None:
-            db_product  = DBProduct(
-                id=product.id,
-                user_id=product.user_id,
-                name=product.name,
-                rating=product.rating,
-                price_with_card=product.price_with_card,
-                price_without_card=product.price_without_card,
-                price_default=product.price_default,
-                link=product.link,
-                url_image=product.url_image,
-                category_product=product.category_product,
-                timestamp=product.timestamp
-            )
-            session.merge(db_product)
+    def save_one_product(self, product: Product, price_stamp: PriceStamp) -> None:
+        db_product  = DBProduct(
+            id=product.id,
+            user_id=product.user_id,
+            name=product.name,
+            rating=product.rating,
+            price_with_card=product.price_with_card,
+            price_without_card=product.price_without_card,
+            price_default=product.price_default,
+            link=product.link,
+            url_image=product.url_image,
+            category_product=product.category_product,
+            timestamp=product.timestamp
+        )
+        session.merge(db_product)
 
-            # Сохраняем клейм цены
-            db_price_stamp = DBPriceStamp(
-                ID_product=product.id,
-                time_stamp=price_stamp.time_stamp,
-                price_with_card=price_stamp.price_with_card,
-                price_without_card=price_stamp.price_without_card,
-                previous_price_without_card=price_stamp.previous_price_without_card,
-                price_default=price_stamp.price_default
-            )
-            session.add(db_price_stamp)
-            session.commit()
+        # Сохраняем клейм цены
+        db_price_stamp = DBPriceStamp(
+            ID_product=product.id,
+            time_stamp=price_stamp.time_stamp,
+            price_with_card=price_stamp.price_with_card,
+            price_without_card=price_stamp.price_without_card,
+            previous_price_without_card=price_stamp.previous_price_without_card,
+            price_default=price_stamp.price_default
+        )
+        session.add(db_price_stamp)
+        session.commit()
 
