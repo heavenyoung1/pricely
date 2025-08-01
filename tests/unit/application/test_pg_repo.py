07c3865d00125b2fe5fcff52095db1954549d6b8
Evgeny <jacksonou3@gmail.com>
2025-08-01 10:23:08 +0300

    Added new checking

--- tests/unit/application/test_pg_repo.py
+++ tests/unit/application/test_pg_repo.py
@@ -16,4 +16,18 @@ def test_product_to_orm_conversion(product_test):
 
     # Assert
     assert isinstance(db_product, DBProduct)  # Проверяем тип
+
+    # Проверка всех полей
     assert db_product.product_id == product_test['product_id']
+    assert db_product.user_id == product_test['user_id']
+    assert db_product.name == product_test['name']
+    assert db_product.rating == pytest.approx(product_test['rating'])  # Для float используем approx
+    assert db_product.price_with_card == product_test['price_with_card']
+    assert db_product.price_without_card == product_test['price_without_card']
+    assert db_product.previous_price_with_card == product_test['previous_price_with_card']
+    assert db_product.previous_price_without_card == product_test['previous_price_without_card']
+    assert db_product.price_default == product_test['price_default']
+    assert db_product.link == product_test['link']
+    assert db_product.url_image == product_test['url_image']
+    assert db_product.category_product == product_test['category_product']
+    assert db_product.last_timestamp == product_test['last_timestamp']
