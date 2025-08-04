5891d56b83fde8df15d26a38ccf6e8de4eb5a392
Evgeny <jacksonou3@gmail.com>
2025-08-04 11:35:53 +0300

    Similar claims for products instead of one claim for all products

--- src/infrastructure/repositories/pg_product_repository.py
+++ src/infrastructure/repositories/pg_product_repository.py
@@ -23,30 +23,27 @@ class PGSQLProductRepository(ProductRepository):
         self.session.add(db_price_stamp)
         self.session.commit()
             
-    def save_few_products(self, products: List, price_stamp: PriceStamp) -> None:
+    def save_few_products(self, products: List[Product], price_stamps: List[PriceStamp]) -> None:
         db_products = [product.to_orm() for product in products]
-        db_price_stamp=price_stamp.to_orm()
+        db_price_stamps = [price_stamp.to_orm() for price_stamp in price_stamps]
         self.session.bulk_save_objects(db_products, update_changed_only=True)
-        self.session.add(db_price_stamp)
+        self.session.add(db_price_stamps)
         self.session.commit()
 
-    def find_product_by_url(self, product_url: str) -> Optional[DBProduct]:
-        db_product_url = self.session.query(DBProduct).filter(DBProduct.link == product_url)
-        if not db_product_url: 
-            raise
-        else:
-            return db_product_url
+    def find_product_by_url(self, product_url: str) -> Optional[Product]:
+        db_product = self.session.query(DBProduct).filter(DBProduct.link == product_url).first()
+        if not db_product:
+            return None
+        return db_product.to_domain()
         
-    def find_product_by_id(self, product_id: str) -> Optional[DBProduct]:
-        db_product_id = self.session.query(DBProduct).filter(DBProduct.product_id == product_id)
-        if not db_product_id: 
-            raise
-        else:
-            return db_product_id
+    def find_product_by_id(self, product_id: str) -> Optional[Product]:
+        db_product = self.session.query(DBProduct).filter(DBProduct.product_id == product_id).first()
+        if not db_product:
+            return None
+        return db_product.to_domain()
         
-    def find_few_products_by_urls(self, product_urls: List[str]) -> List[DBProduct]:
-        for product_url in product_urls:
-            db_products = self.session.query(DBProduct).filter(DBProduct.link.in_(product_urls)).all()
-            return [Product(**db_product.__dict__) for db_product in db_products]
+    def find_few_products_by_urls(self, product_urls: List[str]) -> List[Product]:
+        db_products = self.session.query(DBProduct).filter(DBProduct.link.in_(product_urls)).all()
+        return [db_product.to_domain() for db_product in db_products]
                                                                   
         
