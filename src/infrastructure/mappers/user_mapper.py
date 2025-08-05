f3431f36463300a1cddeaeb7d8961b10b7d89a73
Evgeny <jacksonou3@gmail.com>
2025-08-05 17:00:37 +0300

    lil bug fixed

--- src/infrastructure/mappers/user_mapper.py
+++ src/infrastructure/mappers/user_mapper.py
@@ -1,6 +1,16 @@
+from typing import TYPE_CHECKING
+from src.infrastructure.database.models import ORMUser
+
 if TYPE_CHECKING:
-    from src.domain.entities import 
+    from src.domain.entities import User
 
 class UserMapper:
     @staticmethod
-    def to_orm(user: User)
+    def to_orm(user: User) -> ORMUser:
+        '''Преобразовать User в ORMUser'''
+        return ORMUser(
+            user_ud=user.user_id,
+            username=user.username,
+            chat_id=user.chat_id,
+            product=user.products #НУ ТУТ ТОЧНО НЕ ТАК!!!
+        )
