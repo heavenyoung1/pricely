c696a424da28842c7c8f6c8e029128b0b5f61d5c
Evgeny <jacksonou3@gmail.com>
2025-08-05 15:30:01 +0300

    __init__ files import created

---
+++ src/infrastructure/mappers/__init__.py
@@ -0,0 +1,6 @@
+from .product_mapper import ProductMapper
+from .price_mapper import PriceMapper
+from .price_claim_mapper import PriceClaimMapper
+from .user_mapper import UserMapper
+
+__all__ = ['ProductMapper', 'PriceMapper', 'PriceClaimMapper', 'UserMapper']
