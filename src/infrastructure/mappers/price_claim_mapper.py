b5ec765b57637b1fef31a821f10c9d9ee3019ff3
Evgeny <jacksonou3@gmail.com>
2025-08-06 08:46:50 +0300

    PriceClaimMapper -> bug fixed

--- src/infrastructure/mappers/price_claim_mapper.py
+++ src/infrastructure/mappers/price_claim_mapper.py
@@ -1,20 +1,16 @@
-from typing import TYPE_CHECKING
-
 from src.domain.entities import PriceClaim
 from src.infrastructure.database.models import ORMPriceClaim
 from .price_mapper import PriceMapper
 
-if TYPE_CHECKING:
-    from src.domain.entities import PriceClaim
 
 
 class PriceClaimMapper:
     @staticmethod
     def to_orm(price_claim: PriceClaim) -> ORMPriceClaim:
-        '''Преобразовать PriceClaimMapper в ORMPriceClaim'''
+        '''Преобразовать PriceClaim в ORMPriceClaim'''
         return ORMPriceClaim(
         claim_id=price_claim.claim_id,
         product_id=price_claim.product_id,
-        time_calm=price_claim.time_claim,
+        time_claim=price_claim.time_claim,
         price=PriceMapper.to_orm(price_claim.price, price_claim.claim_id),
         )
