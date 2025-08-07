04895777b3149cbaba0fb0f809e8f096f7de4f89
Evgeny <jacksonou3@gmail.com>
2025-08-07 16:19:41 +0300

    added relationships for ORM models

--- src/infrastructure/database/models/product.py
+++ src/infrastructure/database/models/product.py
@@ -5,16 +5,19 @@ from typing import List, TYPE_CHECKING
 from .base import Base
 
 if TYPE_CHECKING:
-    from .base import Base
-    from .user import ORMUser
+    from . import ORMUser
+    from . import ORMPrice
 
 class ORMProduct(Base):
     __tablename__ = 'products'
     id: Mapped[str] = mapped_column(primary_key=True)
-    user_id: Mapped[str] = mapped_column(ForeignKey('users.user_id'))
+    user_id: Mapped[str] = mapped_column(ForeignKey('users.user_id'), nullable=True)
     price_id: Mapped['ORMUser'] = mapped_column(ForeignKey('prices.id'))
     name: Mapped[str] = mapped_column(String)
     link: Mapped[str] = mapped_column(String)
     image_url: Mapped[str] = mapped_column(String)
     rating: Mapped[float] = mapped_column(Float)
     categories: Mapped[str] = mapped_column(String)  # Храним как JSON
+    user: Mapped['ORMUser'] = relationship('ORMUser', back_populates='products')
+    price: Mapped['ORMPrice'] = relationship('ORMPrice', foreign_keys=[price_id], uselist=False)
+    prices: Mapped[list['ORMPrice']] = relationship('ORMPrice', foreign_keys='ORMPrice.product_id')
