62d33e2175bc53b10deaff267df8bbd1b94c3e62
Evgeny <jacksonou3@gmail.com>
2025-08-07 16:32:04 +0300

    new ORM for new logic

--- src/infrastructure/mappers/user_mapper.py
+++ src/infrastructure/mappers/user_mapper.py
@@ -1,22 +1,29 @@
-from typing import TYPE_CHECKING
+import json
+from sqlalchemy import select
 from sqlalchemy.orm import Session
-from src.infrastructure.database.models import ORMUser
-from .product_mapper import ProductMapper
+from typing import TYPE_CHECKING
+
+from src.infrastructure.database.models import ORMUser, ORMProduct
 
 if TYPE_CHECKING:
     from src.domain.entities import User
 
 class UserMapper:
     @staticmethod
-    def to_orm(user: User, session: Session = None, include_products: bool = False) -> ORMUser:
+    def to_orm(user: User, session: Session = None) -> ORMUser:
         '''Преобразовать User в ORMUser'''
-        orm_user =  ORMUser(
+        orm_user = ORMUser(
             user_id=user.user_id,
             username=user.username,
-            chat_id=user.chat_id,
-            # products=[
-            #     ProductMapper.to_orm(product) for product in user.products
-            # ]
+            chat_id=user.chat_id
         )
-        if include_products and user.products:
-            orm_user.products = [ProductMapper.to_orm(product, session) for product in user.products]
+        # Если products — строка JSON, загружаем продукты
+        if user.products and session:
+            try:
+                product_ids = json.loads(user.products)
+                orm_user.products = session.execute(
+                    select(ORMProduct).where(ORMProduct.id.in_(product_ids))
+                ).scalars().all()
+            except json.JSONDecodeError:
+                raise ValueError('ERROR - Invalid JSON in User.products')
+        return orm_user
