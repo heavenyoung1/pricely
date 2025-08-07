13793c423d038efc909e446ab5f3199977196c9e
Evgeny <jacksonou3@gmail.com>
2025-08-07 16:33:13 +0300

    deleted unuseful code and imports

--- src/infrastructure/repositories/pg_product_repository.py
+++ src/infrastructure/repositories/pg_product_repository.py
@@ -1,36 +1,19 @@
 from sqlalchemy.orm import Session
 from src.interfaces.repositories import ProductRepository
-from src.domain.entities import Product, PriceClaim
+from src.domain.entities import Product
 from src.infrastructure.database.core import with_session
 
-from src.infrastructure.mappers import ProductMapper, PriceClaimMapper
+from src.infrastructure.mappers import ProductMapper
 from sqlalchemy import select
 
 from src.infrastructure.database.models import ORMProduct
 
 class PGSQLProductRepository(ProductRepository):
     @with_session
-    def save_product(self, product: Product, price_claim: PriceClaim, session: Session = None) -> None:
+    def save_product(self, product: Product, session: Session = None) -> None:
         '''Сохранить продукт и связанный ценовой клейм в базе данных'''
         
         # Проверяем, существует ли продукт
         stmt = select(ORMProduct).where(ORMProduct.product_id == product.product_id)
-        existing_product = session.execute(stmt).scalar_one_or_none()
-
-        if existing_product:
-            # Обновляем существующий продукт
-            orm_product = ProductMapper.update_orm(product)
-
-
-            # Добавляем новый ценовой клейм
-            orm_price_claim = PriceClaimMapper(price_claim)
-            existing_product.price_claims.append(price_claim)
-
-        if not existing_product:
-            # Создаем новый продукт с ценовым клеймом
-            orm_product = ProductMapper.to_orm(product)
-            orm_price_claim = PriceClaimMapper.to_orm(price_claim)
-            orm_product.price_claims.append(orm_price_claim)
-            session.add(orm_product)
-                                                                  
+        existing_product = session.execute(stmt).scalar_one_or_none()       
         
