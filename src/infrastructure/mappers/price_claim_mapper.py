e59df70948ce8ddeb58da7c1075650d845b57006
Evgeny <jacksonou3@gmail.com>
2025-08-07 10:59:46 +0300

    update method to_orm

--- src/infrastructure/mappers/price_claim_mapper.py
+++ src/infrastructure/mappers/price_claim_mapper.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import Session
 from src.domain.entities import PriceClaim
 from src.infrastructure.database.models import ORMPriceClaim
 from .price_mapper import PriceMapper
@@ -5,13 +6,12 @@ from .price_mapper import PriceMapper
 
 class PriceClaimMapper:
     @staticmethod
-    def to_orm(price_claim: PriceClaim) -> ORMPriceClaim:
+    def to_orm(price_claim: PriceClaim, session: Session) -> ORMPriceClaim:
         '''Преобразовать PriceClaim в ORMPriceClaim'''
-        return ORMPriceClaim(
+        orm_price_claim =  ORMPriceClaim(
         claim_id=price_claim.claim_id,
         product_id=price_claim.product_id,
         time_claim=price_claim.time_claim,
-        price=PriceMapper.to_orm(price_claim.price, price_claim.claim_id),
         )
-    
-    def update_orm(orm_price_claim: ORMPriceClaim)
+        orm_price_claim.price = PriceMapper.to_orm(price_claim.price, price_claim.claim_id)
+        return orm_price_claim
