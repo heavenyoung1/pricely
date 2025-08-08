bca512c247691defeab3960f6279395826fd89b5
Eugene <jacksonou3@gmail.com>
2025-08-08 20:56:40 +0300

    UserMapper has been implemented

---
+++ src/infrastructure/mappers/user_mapper.py
@@ -0,0 +1,21 @@
+from src.domain.entities import User
+from src.infrastructure.database.models import ORMUser
+
+class UserMapper:
+    @staticmethod
+    def to_orm(entity: User) -> ORMUser:
+        return ORMUser(
+        id=entity.id,
+        username=entity.username,
+        chat_id=entity.chat_id,
+        )
+        # связь products задаём через ORM
+
+    @staticmethod
+    def to_entity(model: ORMUser) -> User:
+        return User(
+            id=model.id,
+            username=model.username,
+            chat_id=model.chat_id,
+            products=[product.id for product in model.products]
+        )
