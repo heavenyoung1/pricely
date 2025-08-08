de1b5f5befd051f1c6f19756d0a737e791ce2b0c
Evgeny <jacksonou3@gmail.com>
2025-08-08 15:52:30 +0300

    changing

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # 📉 Pricely
-
-[Пользователь] → [TelegramBot] → [ProductService] → [Repositories] → [ORM/Models] → [Database]
-    ↑              ↑                  ↑                  ↑              ↑             ↑
-    |              |                  |                  |              |             |
-  Ответ         Создаёт            Вызывает         Преобразуют      Генерируют    Сохраняют
-                сущности          репозитории       в ORM            SQL-запросы   данные
--- src/application/services/product_service.py
+++ src/application/services/product_service.py
@@ -1,8 +1,11 @@
 import logging
-logger = logging.getLogger(__name__)
+import json
 
 from src.domain.entities import Product, Price, User
 from src.interfaces.repositories import ProductRepository, PriceRepository, UserRepository
+
+logger = logging.getLogger(__name__)
+
 
 class ProductService:
     def __init__(
@@ -17,5 +20,25 @@ class ProductService:
 
     def create_product(self, product: Product, price: Price, user: User) -> Product:
         '''Создаёт или обновляет продукт, цену и пользователя.'''
-        self.price_repository.save_
+
+        # Сохраняем цену в PRICE
+        self.price_repository.save_price(price)
+        logger.info(f'Цена сохранена')
+
+        # Обновляем PRICE_ID в PRODUCT 
+        product.price_id = price.id
+        self.product_repository.save_product(product)
+        logger.info(f'Объект PRODUCT обновлен или сохранен')
+
+        product_ids= json.loads(user.products)
+        if product.id not in product_ids:
+            product_ids.append(product.id)
+            user.products = json.dumps(product_ids)
+            logger.info(f'ID Товара добавлен в список к Пользователю')
+
+
+
+
+
+
         
