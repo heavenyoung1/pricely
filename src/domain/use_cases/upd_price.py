164be1b2329339fd3a3458e7808241240768f608
Eugene <jacksonou3@gmail.com>
2025-08-08 22:40:09 +0300

    UpdatePriceUseCase has been implemented

---
+++ src/domain/use_cases/upd_price.py
@@ -0,0 +1,19 @@
+import logging
+from src.domain.repositories import ProductRepository, PriceRepository, UserRepository
+from src.domain.entitites import Product, Price
+
+logger = logging.getLogger(__name__)
+
+# Здесь находится бизнес-логика, которая управляет несколькими сущностями
+
+class UpdatePriceUseCase:
+    def __init__(self, product_repo: ProductRepository, price_repo: PriceRepository):
+        self.product_repo = product_repo
+        self.price_repo = price_repo
+
+    def execute(self, price: Price, product_id: str):
+        self.price_repo.save(price)
+        product = self.product_repo.get(product_id)
+        if product_id:
+            product.price_id = price.id
+            self.product_repo.save(product)
