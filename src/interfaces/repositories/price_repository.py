23ee247898cd68124c3ad3bfd1e2b2cbaea89f50
Evgeny <jacksonou3@gmail.com>
2025-08-08 15:01:13 +0300

    price repo inteface changing

--- src/interfaces/repositories/price_repository.py
+++ src/interfaces/repositories/price_repository.py
@@ -1,16 +1,21 @@
 from abc import ABC, abstractmethod
-from typing import Optional, List, TYPE_CHECKING
-
-from src.domain.entities.product import Product
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from src.domain.entities import Price
 
 class PriceRepository(ABC):
     @abstractmethod
-    def save_price_stamp(self, product: Product, price: Price):
+    def save_price(self, price: Price) -> None:
+        '''Сохранить или обновить цену.'''
         pass
 
     @abstractmethod
-    def delete_stamps_for_product(self, product: Product, price: Price):
+    def get_price(self, price_id: str) -> Price:
+        '''Получить цену по ID.'''
+        pass
+
+    @abstractmethod
+    def get_prices_by_product(self, product_id: str) -> list[Price]:
+        '''Получить все цены для продукта.'''
         pass
