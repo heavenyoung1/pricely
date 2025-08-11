7ceb5682ca5d6c0f483f03addfd668f5a73680db
Evgeny <jacksonou3@gmail.com>
2025-08-11 11:13:35 +0300

    Added decorators for work with DB

--- src/infrastructure/repositories/product_repository.py
+++ src/infrastructure/repositories/product_repository.py
@@ -3,6 +3,7 @@ import logging
 from src.domain.repositories import ProductRepository, PriceRepository, UserRepository
 from src.infrastructure.mappers import ProductMapper, PriceMapper, UserMapper
 from src.infrastructure.database.models import ORMProduct, ORMPrice, ORMUser
+from src.infrastructure.database.core import with_session
 from sqlalchemy.orm import Session
 
 
@@ -12,21 +13,25 @@ class ProductRepositoryImpl(ProductRepository):
     def __init__(self, session: Session):
         self.session = session
 
+    @with_session
     def save(self, product):
         logger.info(f'Продукт сохранен {product}')
         self.session.merge(ProductMapper.to_orm(product))
 
+    @with_session
     def get(self, product_id):
         orm_model = self.session.get(ORMProduct, product_id)
         logger.info(f'Продукт {ProductMapper.to_domain(orm_model)} получен по id: {orm_model.id}')
         return ProductMapper.to_domain(orm_model) if orm_model else None
     
+    @with_session
     def delete(self, product_id):
         orm_model = self.session.get(ORMProduct, product_id)
         logger.info(f'Продукт {ProductMapper.to_domain(orm_model)} получен по id: {orm_model.id}')
         self.session.delete(orm_model) # как дальше удалить, чтобы удалился нужный элемент
 
+    @with_session
     def get_all(self, user_id):
-        orm_model = self.session.get(ORMProduct, user_id)
+        orm_model = self.session.query(ORMProduct).filter_by(user_id=user_id).all()
         logger.info(f'Продукт {ProductMapper.to_domain(orm_model)} получен по user_id: {orm_model.user_id}')
         return ProductMapper.to_domain(orm_model) if orm_model else None
