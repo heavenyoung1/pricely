8b9c793b7439d7e29e45747aa45b0ec012a61ea6
Evgeny <jacksonou3@gmail.com>
2025-08-11 09:42:00 +0300

    Several methods for ProductRepositoryImpl has been implemented, but 1 method not created. 2. added __init__ and here input several imports for correct and useful work 3. changed name of method ( to_entity ->to_domain)

---
+++ src/domain/repositories/__init__.py
@@ -0,0 +1,5 @@
+from .product_repository import ProductRepository
+from .price_repository import PriceRepository
+from .user_repository import UserRepository
+
+__all__ = ['ProductRepository', 'PriceRepository', 'UserRepository']
--- src/infrastructure/mappers/price_mapper.py
+++ src/infrastructure/mappers/price_mapper.py
@@ -17,7 +17,7 @@ class PriceMapper:
         )
 
     @staticmethod
-    def to_entity(model: ORMPrice) -> Price:
+    def to_domain(model: ORMPrice) -> Price:
         return Price(
             id=model.id,
             product_id=model.product_id,
