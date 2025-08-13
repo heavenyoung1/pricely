b50e5871cc16da52bd6a3cf5650ec47acf2cf18d
Evgeny <jacksonou3@gmail.com>
2025-08-13 14:42:00 +0300

    a bit changing of tests, now they're pretty beautifull

--- tests/unit/test_repo_product.py
+++ tests/unit/test_repo_product.py
@@ -9,45 +9,47 @@ from src.infrastructure.mappers import ProductMapper
 import logging
 logger = logging.getLogger(__name__)
 
-# ----- # ----- # ----- Фикстуры ORM слоя ----- # ----- # ----- #
+# ----- # ----- # ----- Тесты ----- # ----- # ----- #
 
 def test_save_product_success(product_repo, product: Product, session):
+    '''Проверяет, что продукт действительно сохранился в БД.'''
     product_repo.save(product)
     saved_product = session.get(ORMProduct, product.id) # Прямой доступ к БД
-    # Здесь можно использовать saved_product как в тесте ниже!!!!
-    assert saved_product is not None
-    assert saved_product.id == product.id
-
-def test_get_product(product_repo, product, session):
-    '''НУ И ПОЧЕМУ ОНИ ПОХОЖИ С ВЕРХНИМ??? '''
-    product_repo.save(product)
-    saved_product = product_repo.get(product_id=product.id)
-    # saved_product лучше чем тот что ниже, так как мы используем методы репозитория
-    #saved_product = session.get(ORMProduct, product.id)
     assert saved_product is not None
     assert saved_product.id == product.id
     assert saved_product.name == product.name
 
-def test_get_product_not_found(product_repo, session):
-    product_id = '6666666'
+def test_get_product(product_repo, product):
+    '''Проверяет, что метод репозитория get корректно возвращает продукт.'''
+    product_repo.save(product)
+    retrieved  = product_repo.get(product_id=product.id) # Через API репозитория
+    assert retrieved is not None
+    assert retrieved.id == product.id
+    assert retrieved.name == product.name
+
+def test_get_product_not_found(product_repo):
+    '''Проверка, что get возвращает None для несуществующего продукта.'''
+    product_id = '666'
     product = product_repo.get(product_id=product_id)
-    assert product == None
+    assert product is None
 
 def test_delete_product(product_repo, product, session):
+    '''Проверка, что delete реально удаляет объект из БД.'''
     product_repo.save(product)
-    saved_product = product_repo.get(product_id=product.id)
-    domain_saved_product = ProductMapper.orm_to_domain(saved_product)
-    logger.debug(f'DOMAIN SAVED PRODUCT {domain_saved_product}')
-    product_repo.delete(domain_saved_product.id)
-    check_deleted_product = product_repo.get(product.id)
-    assert check_deleted_product == None
+    assert session.get(ORMProduct, product.id) is not None  # Есть до удаления
+
+    deleted = product_repo.delete(product.id)
+    assert deleted is True
+    assert session.get(ORMProduct, product.id) is None
 
 def test_delete_product_not_found(product_repo):
+    '''Удаление несуществующего объекта должно вернуть False.'''
     deleted = product_repo.delete('ID_ID')
     assert deleted is False
 
 def test_get_all_products(product_repo, product, user):
+    '''Проверка, что get_all возвращает все товары пользователя.'''
     product_repo.save(product)
-    saved_product_ORM = product_repo.get(product_id=product.id)
     result = product_repo.get_all(user.id)
     assert len(result) == 1
+    assert result[0].id == product.id
