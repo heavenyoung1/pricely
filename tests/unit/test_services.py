957bfafe10a7aadd52ceccf60f2d7a9371c57125
Eugene <jacksonou3@gmail.com>
2025-08-17 19:13:40 +0300

    debug

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -3,7 +3,7 @@ from src.domain.entities import Product, Price, User
 from src.infrastructure.database.models import ORMUser, ORMProduct, ORMUser
 
 def test_create_user_mock(mock_product_service, mock_uow, mock_user_repo, user):
-    '''Провкра логики вызова, НЕ реально сохранение в БД. '''
+    '''ЮНИТ ТЕСТ - Провкра логики вызова, НЕ реально сохранение в БД. '''
     # Настраиваем моки: пользователь не существует
     mock_product_service.create_user(user)
 
@@ -19,3 +19,32 @@ def test_create_user_mock(mock_product_service, mock_uow, mock_user_repo, user):
     created_repo.save.assert_called_once_with(user)
     created_repo.get.assert_not_called()
 
+def test_create_product_mock(mock_product_service, mock_uow, mock_product_repo, product):
+    '''ЮНИТ ТЕСТ - Провкра логики вызова, НЕ реально сохранение в БД. '''
+    # 1. Arrange (подготовка), 
+    # Какая подготовка должна быть? Создание продукта для проверки вызова репозиториев?
+    mock_product_service.create_product(product)
+    created_uow = mock_product_service.uow_factory.return_value
+
+    created_uow.product_repository.return_value
+    
+    created_repo = created_uow.product_
+
+
+
+# def test_create_user_mock(mock_product_service, mock_uow, mock_user_repo, user):
+#     '''ЮНИТ ТЕСТ - Провкра логики вызова, НЕ реально сохранение в БД. '''
+#     # Настраиваем моки: пользователь не существует
+#     mock_product_service.create_user(user)
+
+#     # uow_factory вызван
+#     mock_product_service.uow_factory.assert_called_once()
+#     created_uow = mock_product_service.uow_factory.return_value
+
+#     # user_repository вызван
+#     created_uow.user_repository.assert_called_once_with()
+
+#     # save вызван, get — нет (т.к. use_case просто сохраняет пользователя)
+#     created_repo = created_uow.user_repository.return_value
+#     created_repo.save.assert_called_once_with(user)
+#     created_repo.get.assert_not_called()
--- tests/unit/test_use_case_create_product.py
+++ tests/unit/test_use_case_create_product.py
@@ -5,21 +5,6 @@ from src.domain.entities import Product, Price, User
 from src.infrastructure.mappers import ProductMapper
 from src.infrastructure.database.models import ORMUser
 
-# def test_product_create_success(
-#         session,
-#         product_repo, 
-#         price_repo, 
-#         user_repo, 
-#         product,
-#         price,
-#         user,
-#         ):
-#     # Добавляем пользователя в базу, чтобы пройти проверку existing_user
-#     session.add(ORMUser(id=user.id, username=user.username, chat_id=user.chat_id, products=[]))
-#     session.commit()
-
-#     assert product_repo.get(product.id) is None
-
 # Юнит-тесты с моками
 def test_product_create_success_mock(mock_product_repo, mock_price_repo, mock_user_repo, product, price, user):
     '''Тест успешного создания продукта с моками (юнит-тест).'''
