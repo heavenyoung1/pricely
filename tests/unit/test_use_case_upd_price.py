32e654f785c237bec606f3ecc570e911d6684dc1
Eugene <jacksonou3@gmail.com>
2025-08-22 22:06:51 +0300

    Test has been implemented -> test_upd_price_use_case_success

---
+++ tests/unit/test_use_case_upd_price.py
@@ -0,0 +1,28 @@
+import pytest
+import json
+from src.application.use_cases.upd_price import UpdatePriceUseCase
+from src.domain.entities import Product, Price, User
+from src.infrastructure.mappers import ProductMapper
+from src.infrastructure.database.models import ORMUser
+
+def test_upd_price_use_case_success(
+        mock_product_repo, 
+        mock_price_repo, 
+        product, 
+        price, 
+    ):
+    '''Успешное обновление цены товара.'''
+    mock_product_repo.get.return_value = product
+    mock_price_repo.get.return_value = price
+
+    # Создаём use case
+    use_case = UpdatePriceUseCase(
+        product_repo=mock_product_repo,
+        price_repo=mock_price_repo
+    )
+    use_case.execute(price=price, product_id=product.id)
+
+    mock_price_repo.save.assert_called_once_with(price)
+    mock_product_repo.get.assert_called_once_with(product.id)
+    assert product.price_id == price.id
+    mock_product_repo.save.assert_called_once_with(product)
