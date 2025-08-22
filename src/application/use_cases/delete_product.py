bbfbd367fe83974e74d00614a0d32458fb723ea2
Evgeny <jacksonou3@gmail.com>
2025-08-22 16:44:25 +0300

    test_delete_product_use_case_success has been implemented

--- src/application/use_cases/delete_product.py
+++ src/application/use_cases/delete_product.py
@@ -3,6 +3,10 @@ from src.domain.repositories import ProductRepository, PriceRepository, UserRepo
 
 logger = logging.getLogger(__name__)
 
+class ProductDeletingError(Exception):
+    '''Исключение для ошибок при удалении товара.'''
+    pass
+
 
 class DeleteProductUseCase:
     def __init__(self, user_repo: UserRepository, product_repo: ProductRepository, price_repo: PriceRepository):
@@ -11,20 +15,23 @@ class DeleteProductUseCase:
         self.price_repo = price_repo
 
     def execute(self, product_id: str) -> None:
-        product = self.product_repo.get(product_id)
-        if not product:
-            return
+        try:
+            product = self.product_repo.get(product_id)
+            if not product:
+                raise ValueError(f'Товар {product.id} не существует')
 
-        # удалить цену
-        if product.price_id:
-            self.price_repo.delete(product.price_id)
+            # удалить цену
+            if product.price_id:
+                self.price_repo.delete(product.price_id)
 
-        # удалить товар
-        self.product_repo.delete(product_id)
+            # удалить товар
+            self.product_repo.delete(product_id)
 
-        # удалить связь у пользователя
-        user = self.user_repo.get(product.user_id)
-        if user and product_id in user.products:
-            user.products.remove(product_id)
-            self.user_repo.save(user)
-        logger.info(f'Операция удаления выполнена для ID {product_id}')
+            # удалить связь у пользователя
+            user = self.user_repo.get(product.user_id)
+            if user and product_id in user.products:
+                user.products.remove(product_id)
+                self.user_repo.save(user)
+            logger.info(f'Операция удаления выполнена для ID {product_id}')
+        except Exception as e:
+            raise ProductDeletingError(f'Ошибка удаления товара: {e}')
---
+++ tests/unit/test_use_case_delete_product.py
@@ -0,0 +1,41 @@
+import pytest
+import json
+from src.application.use_cases.delete_product import DeleteProductUseCase, ProductDeletingError
+from src.application.use_cases.create_product import CreateProductUseCase
+from src.domain.entities import Product, Price, User
+from src.infrastructure.mappers import ProductMapper
+from src.infrastructure.database.models import ORMUser
+
+def test_delete_product_use_case_success(
+        mock_product_repo, 
+        mock_price_repo, 
+        mock_user_repo, 
+        product, 
+        price, 
+        user,
+    ):
+    '''Успешное удаление товара с ценой и пользователем.'''
+
+    # Настраиваем моки: продукт существует, пользователь тоже
+    mock_product_repo.get.return_value = product
+    mock_user_repo.get.return_value = user
+
+    use_case = DeleteProductUseCase(
+        product_repo=mock_product_repo,
+        price_repo=mock_price_repo,
+        user_repo=mock_user_repo,
+    )
+
+    # Выполняем удаление
+    use_case.execute(product.id)
+
+    # Проверяем, что цена удалена
+    mock_price_repo.delete.assert_called_once_with(product.price_id)
+
+    # Проверяем, что товар удалён
+    mock_product_repo.delete.assert_called_once_with(product.id)
+
+    assert product.id not in user.products
+    mock_user_repo.save.assert_called_once_with(user)
+
+
