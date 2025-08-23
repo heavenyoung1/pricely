23addee4c74f33d9a610dd00b666091e368fb373
Eugene <jacksonou3@gmail.com>
2025-08-23 23:41:42 +0300

    Error saving DB and exclude logs

--- src/bot/handlers/products.py
+++ src/bot/handlers/products.py
@@ -3,6 +3,7 @@ from requests import head
 import logging
 import sys
 import uuid
+from datetime import datetime
 
 from src.bot.keyboards.main_menu import main_menu
 from src.infrastructure.core.ozon_parser import OzonParser
@@ -51,9 +52,9 @@ def register_handlers(bot):
 
         # Парсинг данных
         try:
-            bot.send_message(message.chat.id, f'Процесс записи данных начат, ожидайте.')
+            bot.send_message(message.chat.id, f'Процесс парсинга данных начат, ожидайте.')
             product_data = parser.parse_product(url)
-            logger.info(f'Успешно спарсены данные для {url}: {product_data}')
+            logger.info(f'Успешный парсинг данных для {url}: {product_data}')
         except Exception as e:
             bot.send_message(message.chat.id, f"Ошибка при парсинге: {str(e)}. Проверь ссылку.", reply_markup=main_menu())
             logger.error(f"Ошибка парсинга URL {url} для пользователя {user_id}: {str(e)}")
@@ -62,7 +63,7 @@ def register_handlers(bot):
         # Создание доменных сущностей
         try:
             product = Product (
-                id = product_data['id'],
+                id=product_data['id'],
                 user_id=user_id,
                 price_id=str(uuid.uuid4()),
                 name=product_data['name'],
@@ -73,8 +74,35 @@ def register_handlers(bot):
             )
 
             price = Price(
-                id 
+                id=product.price_id,
+                product_id=product.id,
+                with_card=product_data['price_with_card'],
+                without_card=product_data['price_without_card'],
+                previous_with_card=product_data['price_default'], # ПРОБЛЕМКА
+                previous_without_card=product_data['price_default'],  # ПРОБЛЕМКА
+                default=product_data['price_default'],
+                claim=datetime.now(),
+            )
+            user = User(
+                id=user_id,
+                username=message.from_user.username,
+                chat_id=user_id,
+                products=[] # А как здесь добавить в список еще один товар???
             )
             
         except Exception as e:
-            raise
+            bot.send_message(message.chat.id, f"Ошибка создания сущностей: {str(e)}.", reply_markup=main_menu())
+            logger.error(f"Ошибка создания сущностей для {url} от пользователя {user_id}: {str(e)}")
+            return
+        
+        try:
+            product_service.create_user()
+            product_service.create_product(user_id, product, price)
+            bot.send_message(message.chat.id, f"Товар {product_data['name']} успешно добавлен!", reply_markup=main_menu())
+            logger.info(f"Данные для {url} сохранены в БД для пользователя {user_id}")
+        except Exception as e:
+            bot.send_message(message.chat.id, f"Ошибка сохранения в БД: {str(e)}. Данные отправлены, но не сохранены.", reply_markup=main_menu())
+            logger.error(f"Ошибка сохранения в БД для {url} от пользователя {user_id}: {str(e)}")
+
+        # Сброс состояния
+        del user_states[message.chat.id]
