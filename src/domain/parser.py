710b514b4f88b4dbcfb09d1e627c93c5fc527253
Eugene <jacksonou3@gmail.com>
2025-08-23 12:03:09 +0300

    Added parser interface

---
+++ src/domain/parser.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+
+class ParserError(Exception):
+    '''Исключение для ошибок парсинга.'''
+    pass
+
+class Parser(ABC):
+    '''Интерфейс для парсера товаров с маркетплейсов.'''
+    
+    @abstractmethod
+    def parse(self, url: str) -> Dict:
+        '''Парсит страницу товара по URL и возвращает данные.
+        
+        Args:
+            url (str): URL страницы товара
+            
+        Returns:
+            Dict: Словарь с данными (title, price, image, и т.д.)
+        
+        Raises:
+            ParserError: Если парсинг провалился
+        '''
+        pass
