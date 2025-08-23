b827f60313513e6fdd7664c9208604f0e3e2a5c2
Eugene <jacksonou3@gmail.com>
2025-08-23 21:29:49 +0300

    Added formatter

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -3,10 +3,10 @@ from src.bot.keyboards.main_menu import main_menu
 
 def register_handlers(bot):
 
-    @bot.message_handler(commands=["start"])
+    @bot.message_handler(commands=['start'])
     def cmd_start(message: Message):
         bot.send_message(
             message.chat.id,
-            "👋 Привет! Я бот для отслеживания цен.\nВыбери действие:",
+            '👋 Привет! Я бот для отслеживания цен.\nВыбери действие:',
             reply_markup=main_menu()
         )
