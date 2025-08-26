a5e1c0843e1ac8bbfa01a28a665b4d5bfc921448
Evgeny <jacksonou3@gmail.com>
2025-08-26 16:26:50 +0300

    mock_parser has been configured

--- tests/conftest.py
+++ tests/conftest.py
@@ -230,3 +230,18 @@ def orm_user(session, orm_product):
     return user
 
 # ----- # ----- # ----- МОК ДЛЯ ПАРСЕРА # ----- # ----- # ----- #
+
+@pytest.fixture
+def mock_parser():
+    parser = MagicMock()
+    parser.parse_product.return_value = {
+        'id': 'p1',
+        'name':'Test Product',
+        'image_url': 'https://example.com/image.jpg',
+        'rating': 4.5,
+        'categories': ['cat1', 'cat2'],
+        'price_with_card': 100,
+        'price_without_card': 120,
+        'price_default': 150,
+    }
+    return parser
