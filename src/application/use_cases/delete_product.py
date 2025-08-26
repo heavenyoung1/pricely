e787057fe6aad41231baaa061833262e388c4f12
Eugene <jacksonou3@gmail.com>
2025-08-26 21:44:40 +0300

    MORE LOGS!!!!

--- src/application/use_cases/delete_product.py
+++ src/application/use_cases/delete_product.py
@@ -3,13 +3,18 @@ from src.domain.repositories import ProductRepository, PriceRepository, UserRepo
 
 logger = logging.getLogger(__name__)
 
+
 class ProductDeletingError(Exception):
     '''Исключение для ошибок при удалении товара.'''
     pass
 
-
 class DeleteProductUseCase:
-    def __init__(self, user_repo: UserRepository, product_repo: ProductRepository, price_repo: PriceRepository):
+    def __init__(
+            self, 
+            user_repo: UserRepository,
+            product_repo: ProductRepository, 
+            price_repo: PriceRepository
+            ):
         self.user_repo = user_repo
         self.product_repo = product_repo
         self.price_repo = price_repo
@@ -18,20 +23,27 @@ class DeleteProductUseCase:
         try:
             product = self.product_repo.get(product_id)
             if not product:
-                raise ValueError(f'Товар {product.id} не существует')
+                logger.warning(f'Товар {product_id} не существует в БД, пропускаем удаление')
+                raise ValueError(f'Товар {product.id} не существует в БД!')
 
-            # удалить цену
+            # Удаление цены, если она существует
             if product.price_id:
                 self.price_repo.delete(product.price_id)
+                logger.debug(f'Цена {product.price_id} удалена для товара {product_id}')
 
-            # удалить товар
+            # Удаление товара
             self.product_repo.delete(product_id)
+            logger.debug(f'Товар {product_id} удален')
 
             # удалить связь у пользователя
             user = self.user_repo.get(product.user_id)
             if user and product_id in user.products:
                 user.products.remove(product_id)
                 self.user_repo.save(user)
+                logger.debug(f'Товар {product_id} удален из списка пользователя {product.user_id}')
+            
             logger.info(f'Операция удаления выполнена для ID {product_id}')
+
         except Exception as e:
-            raise ProductDeletingError(f'Ошибка удаления товара: {e}')
+            logger.error(f'Ошибка при удалении товара {product_id}: {str(e)}')
+            raise ProductDeletingError(f'Ошибка удаления товара: {str(e)}')
