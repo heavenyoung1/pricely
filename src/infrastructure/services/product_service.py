a65b3dc96df20c347a1c6b0e8409438ad4a7fd49
Evgeny <jacksonou3@gmail.com>
2025-08-27 17:10:21 +0300

    DEBUG

--- src/infrastructure/services/product_service.py
+++ src/infrastructure/services/product_service.py
@@ -6,7 +6,7 @@ from src.application.use_cases import (
     CreateProductUseCase,
     GetProductUseCase,
     GetFullProductUseCase,
-    UpdatePriceUseCase,
+    UpdateProductPriceUseCase,
     DeleteProductUseCase,
 )
 
@@ -17,8 +17,23 @@ logger = logging.getLogger(__name__)
 
 class ProductService:
     '''Сервисный слой. Никаких commit здесь руками — коммитит UoW.'''
-    def __init__(self, uow_factory=None):
+    def __init__(
+        self,
+        uow_factory,
+        create_user_use_case: CreateUserUseCase,
+        create_product_use_case: CreateProductUseCase,
+        get_product_use_case: GetProductUseCase,
+        get_full_product_use_case: GetFullProductUseCase,
+        update_product_price_use_case: UpdateProductPriceUseCase,
+        delete_product_use_case: DeleteProductUseCase
+        ):
         self.uow_factory = uow_factory
+        self.create_user_use_case = create_user_use_case
+        self.create_product_use_case = create_product_use_case
+        self.get_product_use_case = get_product_use_case
+        self.get_full_product_use_case = get_full_product_use_case
+        self.update_product_price_use_case = update_product_price_use_case
+        self.delete_product_use_case = delete_product_use_case
 
     @with_uow(commit=True)
     def create_user(self, user: User, uow: UnitOfWork):
@@ -32,13 +47,12 @@ class ProductService:
             product_repo=uow.product_repository(),
             price_repo=uow.price_repository(),
         )
-        print("DEBUG:", use_case.execute)
         return use_case.execute(user_id, url)
 
     @with_uow(commit=False)
     def get_product(self, product_id: str, uow: UnitOfWork) -> Optional[Product]:
         use_case = GetProductUseCase(product_repo=uow.product_repository())
-        use_case.execute(product_id=product_id)
+        use_case.execute(product_id)
 
     @with_uow(commit=False)
     def get_full_product(self, product_id, uow: UnitOfWork) -> Optional[Dict]:
@@ -55,7 +69,7 @@ class ProductService:
             product_repo=uow.product_repository(),
             price_repo=uow.price_repository(),
         )
-        use_case.execute(price, product_id)
+        use_case.execute(product_id, price)
 
     @with_uow(commit=False)
     def delete_product(self, product_id, uow: UnitOfWork) -> None:
