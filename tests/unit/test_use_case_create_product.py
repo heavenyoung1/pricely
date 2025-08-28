138be9d276b3ed2d96a604648a765b804b833c00
Evgeny <jacksonou3@gmail.com>
2025-08-28 15:29:50 +0300

    Test has been completed

--- tests/unit/test_use_case_create_product.py
+++ tests/unit/test_use_case_create_product.py
@@ -7,6 +7,7 @@ from src.domain.entities import Product, Price, User
 from src.infrastructure.mappers import ProductMapper
 from src.infrastructure.database.models import ORMUser
 from src.infrastructure.core.ozon_parser import OzonParser
+from src.application.exceptions import ProductCreationError
 
 
 @patch('src.application.use_cases.create_product.uuid.uuid4')  # Патчим UUID в нужном модуле
@@ -38,7 +39,7 @@ def test_create_product_use_case_success(
         product_repo=mock_product_repo,
         price_repo=mock_price_repo,
         user_repo=mock_user_repo,
-        parser=mock_parser, #без () возвращаем словарь а не объект!
+        parser=mock_parser, #без (), возвращаем словарь а не объект!
     )
     # Выполняем создание продукта
     use_case.execute(user_id=user.id, url=product.link)
@@ -49,67 +50,42 @@ def test_create_product_use_case_success(
 
 
 
-# def test_create_product_use_case_product_exists(
+# @patch('src.application.use_cases.create_product.uuid.uuid4')  # Патчим UUID в нужном модуле
+# @patch('src.application.use_cases.create_product.datetime')  # Патчим datetime
+# def test_create_exist_product_use_case(
+#         mock_datetime,
+#         mock_uuid,
+#         mock_parser,
 #         mock_product_repo, 
 #         mock_price_repo, 
 #         mock_user_repo, 
 #         product, 
 #         price, 
 #         user,
-#         mock_parser,
-# ):
-#     '''Юнит-тест: ошибка, если продукт уже существует.'''
+#     ):
+    
+#     # UUID всегда одинаковый
+#     mock_uuid.return_value = "pr1"
+
+#     # Настраиваем мок datetime для возврата фиксированного времени
+#     mock_datetime.now.return_value = datetime(2025, 1, 1, 0, 0)
+
 #     # Настраиваем моки
-#     mock_user_repo.get.return_value = user      # Пользователь существует
-#     mock_product_repo.get.return_value = product   # Товар не существует
+#     mock_user_repo.get.return_value = None      # Пользователь не существует
+#     mock_product_repo.get.return_value = product   # Товар существует
 
 #     # Создаём use case
 #     use_case = CreateProductUseCase(
 #         product_repo=mock_product_repo,
 #         price_repo=mock_price_repo,
 #         user_repo=mock_user_repo,
-#         parser=mock_parser(),
+#         parser=mock_parser, #без (), возвращаем словарь а не объект!
 #     )
+#     # Выполняем создание продукта
+#     with pytest.raises(ProductCreationError, match=f'Ошибка сохранения товара'):
+#         use_case.execute(user_id=user.id, url=product.link)
 
-#     # Выполняем создание товара
-#     # with pytest.raises(ProductCreationError, match=f'Товар {product.id} уже существует'):
-#     #     use_case.execute(product=product, user_id=user.id, price=price)
-
-#     #mock_user_repo.get.assert_called_once_with(user.id)
-#     mock_product_repo.get.assert_called_once_with(product.id)
-#     assert not mock_price_repo.save.called, 'save (цена) не должен быть вызван'
-#     assert not mock_product_repo.save.called, 'save (продукт) не должен быть вызван'
-#     #assert not mock_user_repo.save.called, 'save (пользователь) не должен быть вызван'
-
-
-# def test_create_product_use_case_user_not_found(
-#         mock_product_repo, 
-#         mock_price_repo, 
-#         mock_user_repo, 
-#         product, 
-#         price, 
-#         user,
-#         mock_parser,
-#         ):
-#     '''Юнит-тест: ошибка, если пользователь не найден.'''
-#     mock_user_repo.get.return_value = None      # Пользователь НЕ существует
-
-#     # Создаём use case
-#     use_case = CreateProductUseCase(
-#         product_repo=mock_product_repo,
-#         price_repo=mock_price_repo,
-#         user_repo=mock_user_repo,
-#         parser=mock_parser(),
-#     )
-
-#     # Выполняем создание товара
-#     # with pytest.raises(ProductCreationError, match=f'Пользователь {user.id} не найден'):
-#     #     use_case.execute(product=product, user_id=user.id, price=price)
-
-#     mock_user_repo.get.assert_called_once_with(user.id)
-#     # Вот эта строка нужна чтобы убедиться, что выполнение метода execute остановилось на нужной строке
-#     # В нашем случае строка 29 -> existing_product = self.product_repo.get(product.id) выполниться не должна
-#     assert not mock_product_repo.get.called, 'get (продукт) не должен быть вызван'
-#     assert not mock_price_repo.save.called, 'save (цена) не должен быть вызван'
-#     assert not mock_product_repo.save.called, 'save (продукт) не должен быть вызван'
-#     assert not mock_user_repo.save.called, 'save (пользователь) не должен быть вызван'
+#     mock_parser.parse_product.assert_called_once_with(product.link)
+#     mock_product_repo.save.assert_not_called()
+#     mock_price_repo.save.assert_not_called()
+    # Этот тест не получится сделать так как нужно проверять это на слое с БД??
--- tests/unit/test_use_case_get_product.py
+++ tests/unit/test_use_case_get_product.py
@@ -4,8 +4,9 @@ from src.application.use_cases.get_product import GetProductUseCase
 from src.domain.entities import Product, Price, User
 from src.infrastructure.mappers import ProductMapper
 from src.infrastructure.database.models import ORMUser
+from src.application.exceptions import ProductNotFoundError
 
-def test_create_product_use_case_success(
+def test_get_product_use_case_success(
         mock_product_repo, 
         product, 
     ):
@@ -31,7 +32,10 @@ def test_get_product_use_case_not_found(mock_product_repo):
     mock_product_repo.get.return_value = None
     use_case = GetProductUseCase(product_repo=mock_product_repo)
 
-    result = use_case.execute(product_id='ID_ID')
+    # Выполняем создание продукта
+    with pytest.raises(ProductNotFoundError, match=f'Продукт ID_ID не существует'):
+        use_case.execute(product_id='ID_ID')
 
-    mock_product_repo.get.assert_called_once_with('ID_ID')
-    assert result is None
+    # Выполняем создание продукта
+    with pytest.raises(ProductNotFoundError, match=f'Идентификатор продукта не указан'):
+        use_case.execute(product_id='')
