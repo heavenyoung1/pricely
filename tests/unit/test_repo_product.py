dec8d0504360b160236a20985558dca2c64e7f63
Evgeny <jacksonou3@gmail.com>
2025-09-04 09:52:25 +0300

    test

--- tests/unit/test_repo_product.py
+++ tests/unit/test_repo_product.py
@@ -3,7 +3,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from src.infrastructure.database.models import ORMProduct
 from src.domain.entities import Product
-from src.infrastructure.database.repositories import PriceRepositoryImpl
+from src.infrastructure.database.repositories import ProductRepositoryImpl
 from src.infrastructure.database.mappers import ProductMapper
 
 import logging
@@ -18,6 +18,38 @@ def test_save_product_success(product_repo, product: Product, db_session):
     assert saved_product is not None
     assert saved_product.id == product.id
     assert saved_product.name == product.name
+
+@pytest.mark.unit
+def test_save_product_success(product, mock_session, orm_product):
+    repo = ProductRepositoryImpl(session=mock_session)
+
+    # 1. Проверяем, что при первом вызове get() вернется None
+    mock_session.get.return_value = None
+    received_empty = repo.get(product_id=product.id)
+    assert received_empty is None
+
+    # 2. Выполняем save()
+    repo.save(product)
+
+    # 3. Теперь подсовываем ORM-модель, которую вернёт get()
+    mock_session.get.return_value = orm_product
+    received = repo.get(product_id=product.id)
+
+    # Asserts
+    assert received is not None
+    assert received.id == product.id
+    assert received.name == product.name
+
+    mock_session.merge.assert_called_once()
+    mock_session.get.assert_called_with(ORMProduct, product.id)
+
+@pytest.mark.unit
+def test_save_price_unsuccess(product, mock_session):
+    repo = ProductRepositoryImpl(session=mock_session)
+    received_empty = repo.get(product_id=product.id)
+    assert received_empty is None
+    #repo.save('NON_PRODUCTS')
+
 
 def test_get_product(product_repo, product):
     '''Проверяет, что метод репозитория get корректно возвращает продукт.'''
@@ -58,11 +90,11 @@ def test_get_all_products(product_repo, product, user):
 
     #АХАХАХ, тут воолбщзе не тот репо
     # ИСПОЛЬЗУЙ PRODUCT REPO!!! 
-@pytest.mark.unit
-def test_get_relevant_price_id_found(mock_session, price, orm_product):
-    repo = PriceRepositoryImpl(session=mock_session)
-    mock_session.get.return_value = orm_product
+# @pytest.mark.unit
+# def test_get_relevant_price_id_found(mock_session, price, orm_product):
+#     repo = PriceRepositoryImpl(session=mock_session)
+#     mock_session.get.return_value = orm_product
 
-    result = repo.get_relevant_price_id(product_id=price.product_id)
-    logger.info(f'RESULT: {result}')
-    assert result == price.id
+#     result = repo.get_relevant_price_id(product_id=price.product_id)
+#     logger.info(f'RESULT: {result}')
+#     assert result == price.id
