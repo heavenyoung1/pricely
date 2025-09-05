cd9d668b122e7c0ab395db3e48bbc27a1c0e513c
Evgeny <jacksonou3@gmail.com>
2025-09-05 10:31:08 +0300

    debug tests

--- tests/unit/test_use_case_create_product.py
+++ tests/unit/test_use_case_create_product.py
@@ -12,79 +12,15 @@ from src.infrastructure.parsers import OzonParser
 import logging
 logger = logging.getLogger(__name__)
 
-# @patch('src.application.use_cases.create_product.uuid.uuid4')
-# @patch('src.application.use_cases.create_product.datetime')
-# def test_create_product_use_case_success(
-#         mock_datetime,
-#         mock_uuid,
-#         mock_parser,
-#         mock_product_repo,
-#         mock_price_repo,
-#         mock_user_repo,
-#         product,
-#         price,
-#         user,
-#         mocker,
-# ):
-#     '''Проверяет успешное создание товара и сохранение пользователя, если он не существует.'''
-#     # Настраиваем моки
-#     mock_uuid.return_value = "pr1"
-#     mock_datetime.now.return_value = datetime(2025, 1, 1, 0, 0)
-#     mocker.patch.object(mock_user_repo, 'get', return_value=None)  # Пользователь не существует
-#     mocker.patch.object(mock_product_repo, 'get', return_value=None)  # Товар не существует
-#     mocker.patch.object(mock_product_repo, 'save', return_value=None)
-#     mocker.patch.object(mock_price_repo, 'save', return_value=None)
-#     mocker.patch.object(mock_user_repo, 'save', return_value=None)
-#     mock_parser.parse_product.return_value = {
-#         'id': product.id,
-#         'name': product.name,
-#         'image_url': product.image_url,
-#         'rating': product.rating,
-#         'categories': product.categories,
-#         'price_with_card': price.with_card,
-#         'price_without_card': price.without_card,
-#         'price_default': price.default,
-#     }
-#     mocker.patch('src.infrastructure.database.mappers.ProductMapper.domain_to_orm', return_value=ProductMapper.domain_to_orm(product))
-#     mocker.patch('src.infrastructure.database.mappers.PriceMapper.domain_to_orm', return_value=PriceMapper.domain_to_orm(price))
-
-#     # Создаем use case
-#     use_case = CreateProductUseCase(
-#         product_repo=mock_product_repo,
-#         price_repo=mock_price_repo,
-#         user_repo=mock_user_repo,
-#         parser=mock_parser,
-#     )
-
-#     # Выполняем создание продукта
-#     result = use_case.execute(user_id=user.id, url=product.link)
-
-#     # Проверяем вызовы
-#     print(f"mock_user_repo.save call count: {mock_user_repo.save.call_count}")
-#     print(f"mock_user_repo.save calls: {mock_user_repo.save.mock_calls}")
-#     mock_parser.parse_product.assert_called_once_with(product.link)
-#     mock_user_repo.get.assert_called_once_with(user.id)
-#     mock_product_repo.get.assert_called_once_with(product.id)
-#     mock_product_repo.save.assert_called_once_with(product)
-#     mock_price_repo.save.assert_called_once_with(price)
-#     mock_user_repo.save.assert_called()
-#     assert mock_user_repo.save.call_count == 2  # Два вызова: для нового пользователя и для обновления списка продуктов
-#     assert result == {
-#         'id': product.id,
-#         'name': product.name,
-#         'user_id': product.user_id,
-#         'price_id': product.price_id
-#     }
-
-def test_create_product_use_case_success(
+def test_create_product_success_new_user(
     pure_mock_product_repo,
     pure_mock_price_repo,
     pure_mock_user_repo,
     pure_mock_parser,
     user,
-    product
         ):
-    
+    # Здесь на настраиваются моки return_value 
+    # потому что они уже определены в фикстуре репозиториев -> None
     use_case = CreateProductUseCase(
         product_repo=pure_mock_product_repo,
         price_repo=pure_mock_price_repo,
@@ -94,6 +30,7 @@ def test_create_product_use_case_success(
     result = use_case.execute(user_id=user.id, url='https://example.com/product')
     logger.debug(f'RESULT USE CASE -> {result}')
 
+
     assert result['product_id'] == 'p1'
     assert result['product_name'] == 'Test Product'
     assert result['user_id'] == 'u1'
@@ -102,9 +39,50 @@ def test_create_product_use_case_success(
     pure_mock_parser.parse_product.assert_called_once_with('https://example.com/product')
     pure_mock_user_repo.get.assert_called_once_with('u1')
     pure_mock_product_repo.get.assert_called_once_with('p1')
+
     # Проверяем сохранение (2 раза для user - создание и обновление списка продуктов)
     assert pure_mock_user_repo.save.call_count == 2
     pure_mock_product_repo.save.assert_called_once()
     pure_mock_price_repo.save.assert_called_once()
 
+def test_create_product_success_existing_user(
+    pure_mock_product_repo,
+    pure_mock_price_repo,
+    pure_mock_user_repo,
+    pure_mock_parser,
+    user,
+        ):
+    pure_mock_user_repo.get.return_value = user
+    use_case = CreateProductUseCase(
+        product_repo=pure_mock_product_repo,
+        price_repo=pure_mock_price_repo,
+        user_repo=pure_mock_user_repo,
+        parser=pure_mock_parser,
+    )
+    result = use_case.execute(user_id=user.id, url='https://example.com/product')
 
+    # Проверяем результат
+    assert result['user_id'] == user.id
+
+    # Пользователь сохраняется только 1 раз (обновление списка продуктов)
+    pure_mock_user_repo.save.assert_called_once()
+
+def test_create_product_fails_product_exists(
+    pure_mock_product_repo,
+    pure_mock_price_repo, 
+    pure_mock_user_repo,
+    pure_mock_parser,
+    product
+):
+    '''Тест: товар уже существует'''
+    pure_mock_product_repo.get.return_value = product
+    
+    use_case = CreateProductUseCase(
+        product_repo=pure_mock_product_repo,
+        price_repo=pure_mock_price_repo,
+        user_repo=pure_mock_user_repo,
+        parser=pure_mock_parser,
+    )
+    
+    with pytest.raises(ProductCreationError, match='уже существует'):
+        use_case.execute(user_id='u1', url='https://example.com/product')
