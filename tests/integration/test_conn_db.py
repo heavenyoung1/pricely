ced51f4065c9295818c1361c0775f6280f3215d3
Evgeny <jacksonou3@gmail.com>
2025-09-08 09:37:04 +0300

    docker deploying

--- tests/integration/test_conn_db.py
+++ tests/integration/test_conn_db.py
@@ -4,16 +4,27 @@ from src.core.db_config import DataBaseSettings
 
 
 @pytest.mark.integration
-def test_database_connection():
+def test_connection_to_both_databases():
     """
     Интеграционный тест:
-    Проверяет, что можно подключиться к БД и выполнить простой запрос.
+    Проверяет, что можно подключиться к основной и тестовой БД.
     """
 
     settings = DataBaseSettings()
-    db = DatabaseConnection(settings)
 
-    assert db.test_connection(), (
-        "Не удалось подключиться к БД. "
-        "Проверьте .env конфиг или доступность сервера PostgreSQL."
+    # Проверка основной базы
+    dev_db = DatabaseConnection(settings)
+    assert dev_db.test_connection(), (
+        "Не удалось подключиться к основной БД. "
+        "Проверьте .env или доступность контейнера postgres_dev."
+    )
+
+    # Проверка тестовой базы
+    if not settings.is_test_db_configured:
+        pytest.skip("Тестовая БД не настроена в .env")
+    test_settings = DataBaseSettings(NAME=settings.TEST_NAME)
+    test_db = DatabaseConnection(test_settings)
+    assert test_db.test_connection(), (
+        "Не удалось подключиться к тестовой БД. "
+        "Проверьте .env или доступность контейнера postgres_test."
     )
