49bbd5059e4257f55ce8f2480ce45e2634a514f1
Evgeny <jacksonou3@gmail.com>
2025-09-08 11:45:54 +0300

    debug

--- scripts/db_url.py
+++ scripts/db_url.py
@@ -1,5 +1,4 @@
-import sys
-import os
+import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # чтобы видеть src
 
 from src.core.db_config import DataBaseSettings
@@ -7,11 +6,6 @@ from src.core.db_config import DataBaseSettings
 if __name__ == "__main__":
     db = DataBaseSettings()
     if len(sys.argv) > 1 and sys.argv[1] == "test":
-        url = db.get_db_url()
-        # Замените postgresql:// на postgresql+psycopg2://
-        url = url.replace("postgresql://", "postgresql+psycopg2://")
-        print(url)
+        print(db.get_alembic_url(use_test=True))
     else:
-        url = db.get_db_url()
-        url = url.replace("postgresql://", "postgresql+psycopg2://")
-        print(url)
+        print(db.get_alembic_url(use_test=False))
