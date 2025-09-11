3625caac2abde935d0ba0360f80eff1bcfca2c8e
Evgeny <jacksonou3@gmail.com>
2025-09-11 10:56:42 +0300

    product_mapper refactoring

--- src/infrastructure/database/mappers/product_mapper.py
+++ src/infrastructure/database/mappers/product_mapper.py
@@ -1,4 +1,5 @@
 from pydantic import HttpUrl
+from datetime import datetime
 import json
 from src.domain.entities import Product
 from src.application.dto import ProductDTO
@@ -11,7 +12,7 @@ class ProductMapper:
         return Product(
             id=dto.id,
             user_id=dto.user_id,
-            price_id='',  # заполнит UseCase
+            price_id='',  # Заполняется в UseCase
             name=dto.name,
             link=str(dto.link),
             image_url=str(dto.image_url),
@@ -25,7 +26,7 @@ class ProductMapper:
             id=domain.id,
             user_id=domain.user_id,
             name=domain.name,
-            link=HttpUrl(domain.link),  # Конвертируем строку в HttpUrl
+            link=HttpUrl(domain.link),
             image_url=HttpUrl(domain.image_url),
             rating=domain.rating,
             categories=domain.categories,
@@ -35,24 +36,23 @@ class ProductMapper:
     def domain_to_orm(domain: Product) -> ORMProduct:
         return ORMProduct(
             id=domain.id,
-            user_id=domain.user_id,
-            price_id=domain.price_id,
             name=domain.name,
             link=domain.link,
             image_url=domain.image_url,
             rating=domain.rating,
-            categories=json.dumps(domain.categories),  # Сериализуем список в JSON строку
+            categories=json.dumps(domain.categories),  # Сериализуем список в JSON
+            created_at=datetime.now(),  # Устанавливаем текущее время
         )
 
     @staticmethod
     def orm_to_domain(orm: ORMProduct) -> Product:
         return Product(
             id=orm.id,
-            user_id=orm.user_id,
-            price_id=orm.price_id,
+            user_id='',  # Должно заполняться в UseCase или репозитории
+            price_id='',  # Должно заполняться в UseCase или репозитории
             name=orm.name,
             link=orm.link,
             image_url=orm.image_url,
             rating=orm.rating,
-            categories=json.loads(orm.categories) if orm.categories else []  # Десериализуем JSON строку
+            categories=json.loads(orm.categories) if orm.categories else [],
         )
