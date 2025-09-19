77bd3f92fb9eac1d7202ead6cea466e05e3bd99e
Evgeny <jacksonou3@gmail.com>
2025-09-19 13:43:09 +0300

    debug

--- scripts/db_url.py
+++ scripts/db_url.py
@@ -5,8 +5,14 @@ from src.core.db_config import DataBaseSettings
 
 if __name__ == "__main__":
     db = DataBaseSettings()
-    if len(sys.argv) > 1 and sys.argv[1] == "test":
-        print(db.get_alembic_url(use_test=True))
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "test":
+            print(db.get_alembic_url(use_test=True))
+        elif sys.argv[1] == "dev":
+            print(db.get_alembic_url(use_test=False))
+        else:
+            raise ValueError(f"Неизвестный аргумент: {sys.argv[1]} (ожидался 'dev' или 'test')")
     else:
+        # по умолчанию dev
         print(db.get_alembic_url(use_test=False))
 
