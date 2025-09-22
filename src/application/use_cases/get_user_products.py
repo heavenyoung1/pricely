007578288347d0d9d12f2a7fdfc2ed3c378462e5
Evgeny <jacksonou3@gmail.com>
2025-09-22 08:19:53 +0300

    repo method has been created

---
+++ src/application/use_cases/get_user_products.py
@@ -0,0 +1,26 @@
+from src.application.interfaces.repositories import ProductRepository, PriceRepository, UserRepository, UserProductsRepository
+from src.domain.entities import Product, Price, User
+
+from src.infrastructure.parsers import OzonParser
+from datetime import datetime
+import logging
+import uuid
+from src.infrastructure.parsers.interfaces import Parser
+from src.application.interfaces import ProductParser
+from src.domain.exceptions import ParserProductError, ProductCreationError
+from typing import List
+
+logger = logging.getLogger(__name__)
+
+class GetUserProductsUseCase:
+    def __init__(
+        self,
+        user_products_repo: UserProductsRepository,
+    ):
+        self.user_products_repo = user_products_repo
+
+    def execute(self, user_id: str) -> List[str]:
+        logger.info(f'Запрос списка товар для пользователя ID: {user_id}')
+        
+
+    
--- src/infrastructure/database/repositories/user_products_repository.py
+++ src/infrastructure/database/repositories/user_products_repository.py
@@ -2,7 +2,7 @@ import logging
 
 from src.application.interfaces.repositories import UserProductsRepository
 from src.infrastructure.database.mappers import UserMapper
-from src.infrastructure.database.models import ORMUser
+from src.infrastructure.database.models import ORMUserProducts
 
 from sqlalchemy.orm import Session
 
@@ -15,4 +15,12 @@ class UserProductsRepositoryImpl(UserProductsRepository):
         self.session = session
 
     def get_products_for_user(self, user_id):
-        pass
+        '''
+        Возвращает список product_id, связанных с пользователем.
+        '''
+        logger.debug(f"Получение списка продуктов для пользователя {user_id}")
+        rows = (
+        self.session.query(ORMUserProducts).filter(ORMUserProducts.user_id == user_id).all()
+        )
+        logger.debug([row for row in rows])
+        return [row for row in rows]
