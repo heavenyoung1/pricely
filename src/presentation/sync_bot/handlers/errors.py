587a7636c3d9227431538707833bf1895c7bdd8a
Evgeny <jacksonou3@gmail.com>
2025-10-03 18:31:53 +0300

    deleted old version bot

--- src/presentation/sync_bot/handlers/errors.py
+++
@@ -1,9 +0,0 @@
-from telebot.types import Message
-from src.presentation.sync_bot.bot_instance import bot
-
-@bot.message_handler(func=lambda m: True)
-def fallback(message: Message):
-    bot.send_message(message.chat.id, "❓ Неизвестная команда!!!. Напишите /help")
-
-def register_handlers(bot):
-    bot.message_handler(func=lambda m: True)(fallback)
--- src/presentation/sync_bot/service_connector.py
+++
@@ -1,4 +0,0 @@
-from src.infrastructure.services.product_service import ProductService
-from src.core import SQLAlchemyUnitOfWork
-
-service = ProductService(uow_factory=SQLAlchemyUnitOfWork)
