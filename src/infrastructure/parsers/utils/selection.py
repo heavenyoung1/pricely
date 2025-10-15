7684901fa171a0dfb804db248762d641847b3815
Evgeny <jacksonou3@gmail.com>
2025-10-15 12:54:34 +0300

    Внедрение UserAgent'ов и Proxy

---
+++ src/infrastructure/parsers/utils/selection.py
@@ -0,0 +1,39 @@
+import random 
+import json
+import logging
+from .user_agents import user_agents_list as user_agents
+
+logger = logging.getLogger(__name__)
+
+def get_user_agent():
+    try:
+        index = random.randrange(len(user_agents))
+        user_agent = user_agents[index]
+        logger.info(f'UserAgent успешно получен {user_agent}')
+        return user_agent
+    except Exception as e:
+        logger.error(f'Ошибка получения UserAgent: {e}')
+        raise
+
+# Загрузка прокси из файла
+def load_proxies():
+    try:
+        with open("proxy.json", "r") as file:
+            proxies = json.load(file)
+        return proxies
+    except Exception as e:
+        logger.error(f'Ошибка считывания Прокси: {e}')
+        raise
+
+def get_proxy():
+    try:
+        proxies = load_proxies()
+        proxy_data = random.choice(proxies)
+        proxy = proxy_data['proxy']
+        user = proxy_data['user']
+        password = proxy_data['password']
+
+        return proxy, user, password
+    except Exception as e:
+        logger.error(f'Ошибка получения Прокси: {e}')
+        raise
