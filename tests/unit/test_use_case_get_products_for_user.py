970b1d395f24acf81724ff8793bdb832c7925545
Evgeny <jacksonou3@gmail.com>
2025-10-20 14:27:20 +0300

    Добавление тестового сценария для проверки отсусттвия строк при извлечении товаров для пользователя

--- tests/unit/test_use_case_get_products_for_user.py
+++ tests/unit/test_use_case_get_products_for_user.py
@@ -25,3 +25,29 @@ def test_get_product_for_user_success(
     # Проверяем, что метод для получения товаров был вызван один раз
     pure_mock_user_products_repo.get_products_for_user.assert_called_once_with("user_123")
     pure_mock_product_repo.get.assert_not_called()  # Не должны были запрашиваться подробности о товарах
+
+@pytest.mark.unit
+def test_get_product_for_user_no_products(
+    pure_mock_user_products_repo,
+    pure_mock_product_repo,
+    pure_mock_price_repo,
+):
+    # Настройка мока для репозитория привязки товаров и пользователей
+    pure_mock_user_products_repo.get_products_for_user.return_value = []
+
+    # Создаем UseCase
+    use_case = GetProductForUserUseCase(
+        user_products_repo=pure_mock_user_products_repo,
+        product_repo=pure_mock_product_repo,
+        price_repo=pure_mock_price_repo,
+    )  
+
+    # Вызов метода
+    result = use_case.execute(user_id="user_123")
+
+    # Проверяем, что метод для получения товаров был вызван один раз
+    pure_mock_user_products_repo.get_products_for_user.assert_called_once_with("user_123")
+    pure_mock_product_repo.get.assert_not_called()  # Не должны были запрашиваться подробности о товарах
+
+    # Проверяем, что возвращается пустой список, так как товаров нет
+    assert result == []
