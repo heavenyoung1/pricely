c81c7fc6092ac5bfb1f2dc06a3a427ea6fd2f2df
Evgeny <jacksonou3@gmail.com>
2025-10-20 12:29:33 +0300

    GetProductUseCase покрыт тестами

--- tests/unit/test_use_case_get_product.py
+++ tests/unit/test_use_case_get_product.py
@@ -32,3 +32,17 @@ def test_get_product_use_case_unsuccess_not_found(
         use_case.execute(product_id=product.id)
     
     pure_mock_product_repo.get.assert_called_once_with(product.id)
+
+@pytest.mark.unit
+def test_get_product_use_case_unsuccess_empty_product_id(
+    pure_mock_product_repo,
+):
+    # Мок не настраиваем, в фикстуре уже None
+
+    use_case = GetProductUseCase(
+        product_repo=pure_mock_product_repo,
+    )
+    with pytest.raises(ProductNotFoundError, match=f'Идентификатор продукта не указан'):
+        use_case.execute(product_id=None)
+    
+    #pure_mock_product_repo.get.assert_called_once_with(product.id)
